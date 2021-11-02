190853506e2cc95c99d854d53262623afc2b6431
ohgeun <ogkim@turtleship.io>
2021-11-02 18:08:12 +0900

    Delete : React.StrictMode 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Router from "router";
 import "antd/dist/antd.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Router />, document.getElementById("root"));
