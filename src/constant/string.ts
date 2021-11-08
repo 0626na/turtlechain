66cccbb71419fb619adb8b66f4877b02720ed686
ohgeun <ogkim@turtleship.io>
2021-11-08 15:40:26 +0900

    Create : 서비스 신청 관련 문자열 추가

--- src/constant/string.ts
+++ src/constant/string.ts
@@ -23,9 +23,31 @@ export const VERIFY_AUTH_NUM = "인증번호 확인";
 // 우편번호 & 주소찾기
 export const FIND_ADDRESS = "주소 찾기";
 export const FIND_POSTCODE = "우편번호 찾기";
+export const DETAIL_ADDRESS = "상세 주소";
+
+// 사업자
+export const BUSINESS_INFORMATION = "사업자 정보";
+export const BUSINESS_NAME = "사업자명";
+export const BUSINESS_NUMBER = "사업자 등록번호";
+export const BUSINESS_ADDRESS = "사업자 주소";
+export const BUSINESS_LICENSE = "사업자 등록증";
+
+// 계정
+export const ADMIN_ACCOUNT = "관리자 계정";
+export const USER_NAME = "사용자명";
+
+// 서비스 신청
+export const SIGN_UP = "서비스 신청하기";
+export const SIGN_UP_SUCCESS = "서비스 신청 완료";
+
+// 단계
+export const PREV = "이전";
+export const NEXT = "다음";
 
 // 기타
+export const ETC = "기타 사항";
+export const GO_HOME = "홈으로";
+export const EMAIL = "이메일";
 export const CLOSE = "닫기";
-export const SIGN_UP = "서비스 신청하기";
 export const FIND_MEMBERSHIP = "멤버쉽 알아보기";
 export const ONLY_NUMBER = "숫자만";
