8aeee81e77490aaeeff9fe051d21c35e8ee34949
ohgeun <ogkim@turtleship.io>
2021-11-08 15:38:58 +0900

    Create : IMAGE_FORM_DESCRIPTION 추가

--- src/constant/description.ts
+++ src/constant/description.ts
@@ -10,3 +10,7 @@ export const FIND_ID_PAGE = {
 export const RESET_PASSWORD_PAGE = {
   AUTH_PHONE_DESCRIPTION: "휴대번호 인증버튼을 눌러 휴대번호를 인증해 주세요.",
 };
+
+// 이미지 양식
+export const IMAGE_FORM_DESCRIPTION =
+  "이미지 양식은 jpg, jpeg, png, pdf만 가능합니다.";
