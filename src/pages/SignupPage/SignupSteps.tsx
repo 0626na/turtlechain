62a9cca25ae185734173e41ae68888a99debd244
ohgeun <ogkim@turtleship.io>
2021-11-08 12:02:10 +0900

    Update : SignupSteps size small 수정

--- src/pages/SignupPage/SignupSteps.tsx
+++ src/pages/SignupPage/SignupSteps.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const SignupSteps = function ({ steps, current }: Props) {
   return (
-    <Steps style={{ padding: "40px 0" }} current={current}>
+    <Steps style={{ padding: "40px 0" }} size="small" current={current}>
       {steps.map((title, index) => {
         return <Steps.Step key={index} title={title} />;
       })}
