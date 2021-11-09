84825642e771bf54a59ce3596bf91f859905b54a
ohgeun <ogkim@turtleship.io>
2021-11-09 19:06:00 +0900

    Update : SignupPage 컴포넌트 currentStep 렌더링 방식 변경
    폼 컴포넌트 배열 추가

--- src/pages/SignupPage/index.tsx
+++ src/pages/SignupPage/index.tsx
@@ -1,10 +1,5 @@
 import { useState } from "react";
 import { Helmet } from "react-helmet";
-import {
-  BUSINESS_INFORMATION,
-  ADMIN_ACCOUNT,
-  SIGN_UP_SUCCESS,
-} from "constant/string";
 import SignupPageTemplate from "./SignupPageTemplate";
 import SignupSteps from "./SignupSteps";
 import CompanyForm from "./CompanyForm";
@@ -22,28 +17,17 @@ const SignupPage = function () {
     setCurrentStep(currentStep + 1);
   };
 
+  const steps = [
+    <CompanyForm onNextStep={handleNextStep} />,
+    <AdminForm onPrevStep={handlePrevStep} onNextStep={handleNextStep} />,
+    <SignupResult />,
+  ];
+
   return (
     <SignupPageTemplate>
       <Helmet title="터틀체인 - 서비스 가입신청" />
-      <SignupSteps
-        steps={[BUSINESS_INFORMATION, ADMIN_ACCOUNT, SIGN_UP_SUCCESS]}
-        current={currentStep}
-      />
-      {currentStep === 0 && (
-        <CompanyForm //
-          onNextStep={handleNextStep}
-        />
-      )}
-      {currentStep === 1 && (
-        <AdminForm //
-          onPrevStep={handlePrevStep}
-          onNextStep={handleNextStep}
-        />
-      )}
-      {currentStep === 2 && (
-        <SignupResult //
-        />
-      )}
+      <SignupSteps current={currentStep} />
+      {steps[currentStep]}
     </SignupPageTemplate>
   );
 };
