991490185637ddf11fdd41b49bf3a5e1c8e0aa48
ohgeun <ogkim@turtleship.io>
2021-11-15 10:17:27 +0900

    Update : 사업자 정보 폼 입력 기능 추가

--- src/pages/SignupPage/CompanyForm.tsx
+++ src/pages/SignupPage/CompanyForm.tsx
@@ -1,33 +1,30 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { Company } from "pages/SignupPage";
 import {
-  BUSINESS_TYPE,
-  CORPORATE_BUSINESS,
-  INDIVIDUAL_BUSINESS,
   OWNER_NAME,
   BUSINESS_NAME,
   BUSINESS_NUMBER,
   BUSINESS_ADDRESS,
   BUSINESS_LICENSE,
   FIND_ADDRESS,
-  DETAIL_ADDRESS,
   ETC,
   PREV,
   NEXT,
 } from "constant/string";
-import { IMAGE_FORM_DESCRIPTION } from "constant/description";
-import { FileImageOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Upload, Typography, Radio } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Upload } from "antd";
 import PostcodeModal from "components/DaumPostcodeModal";
 
 interface Props {
-  onNextStep: () => void;
+  company: Company;
+  setCompany: React.Dispatch<React.SetStateAction<Company>>;
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const CompanyForm = function ({ onNextStep }: Props) {
+const CompanyForm = function ({ company, setCompany, setCurrentStep }: Props) {
   const history = useHistory();
-  const [form] = Form.useForm();
   const [visiblePostcodeModal, setVisiblePostcodeModal] = useState(false);
 
   // 주소 찾기 모달 열기
@@ -42,12 +39,41 @@ const CompanyForm = function ({ onNextStep }: Props) {
 
   // 이전 단계
   const handlePrev = () => {
-    history.goBack();
+    history.push("/");
   };
 
   // 다음 단계
   const handleNext = () => {
-    onNextStep();
+    setCurrentStep((prevStep) => prevStep + 1);
+  };
+
+  // 주소 얻기
+  const getAddress = (address: string) => {
+    setCompany({ ...company, address });
+  };
+
+  // input change 이벤트
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCompany({ ...company, [name]: value });
+  };
+
+  // textarea change 이벤트
+  const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setCompany({ ...company, [name]: value });
+  };
+
+  // upload props
+  const uploadProps = {
+    fileList: company.biz_license_file ? [company.biz_license_file as any] : [],
+    onRemove: () => {
+      setCompany({ ...company, biz_license_file: null });
+    },
+    beforeUpload: (file: File) => {
+      setCompany({ ...company, biz_license_file: file });
+      return false;
+    },
   };
 
   return (
@@ -55,71 +81,81 @@ const CompanyForm = function ({ onNextStep }: Props) {
       <PostcodeModal
         visible={visiblePostcodeModal}
         onClose={closePostcodeModal}
+        onGetAddress={getAddress}
       />
-      <Form form={form} layout="vertical">
-        <Form.Item label={BUSINESS_TYPE}>
-          <Radio.Group>
-            <Radio>{CORPORATE_BUSINESS}</Radio>
-            <Radio>{INDIVIDUAL_BUSINESS}</Radio>
-          </Radio.Group>
-        </Form.Item>
+      <Form layout="vertical">
         <Form.Item label={OWNER_NAME}>
-          <Input />
+          <Input //
+            name="owner"
+            value={company.owner}
+            onChange={handleInputChange}
+          />
         </Form.Item>
         <Form.Item label={BUSINESS_NAME}>
-          <Input />
+          <Input //
+            name="name"
+            value={company.name}
+            onChange={handleInputChange}
+          />
         </Form.Item>
         <Form.Item label={BUSINESS_NUMBER}>
-          <Input />
+          <Input //
+            name="biz_num"
+            value={company.biz_num}
+            onChange={handleInputChange}
+          />
         </Form.Item>
         <Form.Item label={BUSINESS_ADDRESS}>
-          <HorizontalContainer>
-            <Input readOnly />
-            <Button type="primary" onClick={openPostcodeModal}>
-              {FIND_ADDRESS}
-            </Button>
-          </HorizontalContainer>
-        </Form.Item>
-        <Form.Item>
-          <Input placeholder={DETAIL_ADDRESS} />
+          <Input
+            readOnly
+            name="address"
+            value={company.address}
+            suffix={
+              <Button type="link" onClick={openPostcodeModal}>
+                {FIND_ADDRESS}
+              </Button>
+            }
+          />
         </Form.Item>
         <Form.Item label={BUSINESS_LICENSE}>
-          <Upload.Dragger>
-            <ImageIcon />
-            <Typography.Text>{IMAGE_FORM_DESCRIPTION}</Typography.Text>
-          </Upload.Dragger>
+          <Upload listType="picture" {...uploadProps}>
+            <Button
+              type="primary"
+              disabled={company.biz_license_file !== null}
+              icon={<UploadOutlined />}
+            >
+              {BUSINESS_LICENSE}
+            </Button>
+          </Upload>
         </Form.Item>
         <Form.Item label={ETC}>
-          <Input.TextArea style={{ height: 150 }} />
+          <Input.TextArea
+            style={{ height: 150 }}
+            name="memo"
+            value={company.memo}
+            onChange={handleTextAreaChange}
+          />
         </Form.Item>
-        <Form.Item>
-          <HorizontalContainer>
-            <Button block onClick={handlePrev}>
-              {PREV}
-            </Button>
-            <Button block type="primary" onClick={handleNext}>
-              {NEXT}
-            </Button>
-          </HorizontalContainer>
-        </Form.Item>
+        <ButtonContainer>
+          <Button block onClick={handlePrev}>
+            {PREV}
+          </Button>
+          <Button block type="primary" onClick={handleNext}>
+            {NEXT}
+          </Button>
+        </ButtonContainer>
       </Form>
     </>
   );
 };
 
-const HorizontalContainer = styled.div`
+const ButtonContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   & > * + * {
     margin-left: 20px;
   }
-`;
-
-const ImageIcon = styled(FileImageOutlined)`
-  font-size: 3rem;
-  display: block;
-  margin: 20px 0;
 `;
 
 export default CompanyForm;
