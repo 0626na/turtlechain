744181671976fa9e89ae54b3f50e791c23de1feb
ohgeun <ogkim@turtleship.io>
2021-11-15 16:12:58 +0900

    Update : 유저 생성 API 추가

--- src/apis/userAPI.ts
+++ src/apis/userAPI.ts
@@ -15,8 +15,29 @@ const dupCheck = async function (data: RequestDupCheck) {
   return response.data.data;
 };
 
+// 유저 생성
+interface RequestCreate {
+  mobile_tel: string;
+  name: string;
+  email: string;
+  login_id: string;
+  password: string;
+  company_id: number;
+}
+
+interface ResponseCreate {
+  data: null;
+}
+
+const create = async function (data: RequestCreate) {
+  const url = "/auth/user";
+  const response = await v2Axios.post<ResponseCreate>(url, data);
+  return response.data.data;
+};
+
 const userAPI = {
   dupCheck,
+  create,
 };
 
 export default userAPI;
