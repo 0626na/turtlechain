f688ccc94ae9b6966df85442d30ba3a1ea2afcfa
ohgeun <ogkim@turtleship.io>
2021-11-25 16:34:40 +0900

    Create : MyStorePageHeader 컴포넌트 추가

---
+++ src/pages/MyStorePage/MyStorePageHeader.tsx
@@ -0,0 +1,22 @@
+// lang
+import { useTranslation } from "react-i18next";
+// antd
+import { InfoCircleOutlined as InfoIcon } from "@ant-design/icons";
+import { PageHeader, Typography } from "antd";
+
+const MyStorePageHeader = function () {
+  const { t } = useTranslation();
+  return (
+    <PageHeader title={t("mall info")}>
+      <Typography.Text type="secondary">
+        <InfoIcon /> {t("description.first way to manage malls")}
+      </Typography.Text>
+      <br />
+      <Typography.Text type="secondary">
+        <InfoIcon /> {t("description.second way to manage malls")}
+      </Typography.Text>
+    </PageHeader>
+  );
+};
+
+export default MyStorePageHeader;
