0f7ad3d2c4ea7541ec40a9a772867759543814b8
ohgeun <ogkim@turtleship.io>
2021-11-26 13:01:54 +0900

    Update : useLogout 수정

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,12 +1,18 @@
 import { useResetRecoilState } from "recoil";
 import { tokenState } from "store/tokenState";
 import { TOKEN } from "constant";
+import { v1Axios, v2Axios } from "apis";
+import { useQueryClient } from "react-query";
 
 const useLogout = function () {
+  const queryClient = useQueryClient();
   const resetToken = useResetRecoilState(tokenState);
   const logout = () => {
-    resetToken();
+    v1Axios.defaults.headers.common["Authorization"] = "";
+    v2Axios.defaults.headers.common["Authorization"] = "";
+    queryClient.refetchQueries();
     localStorage.removeItem(TOKEN);
+    resetToken();
   };
 
   return { logout };
