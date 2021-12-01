b58d8dab594ca89b5e3bb47863240a9d5cd351ab
ohgeun <ogkim@turtleship.io>
2021-12-01 16:59:45 +0900

    Update : 한글 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import { QueryClientProvider, QueryClient } from "react-query";
 import Router from "router";
+import { ConfigProvider } from "antd";
+import koKR from "antd/es/locale/ko_KR";
+import "moment/locale/ko";
 import "antd/dist/antd.less";
 import "./i18n";
 
@@ -12,7 +15,9 @@ const queryClient = new QueryClient({
 ReactDOM.render(
   <RecoilRoot>
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <ConfigProvider locale={koKR}>
+        <Router />
+      </ConfigProvider>
     </QueryClientProvider>
   </RecoilRoot>,
   document.getElementById("root")
