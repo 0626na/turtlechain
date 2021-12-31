8c5e4c5cc57cf6d2865c0bc8199ca1310a7b085c
정보승 <bsjung@turtleship.io>
2021-12-31 12:49:30 +0900

    Setting : styled-components를 이용한 전역 스타일 설정
    - 전역 폰트 기본값 'Note Sans KR'로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -7,18 +7,28 @@ import koKR from "antd/es/locale/ko_KR";
 import "moment/locale/ko";
 import "antd/dist/antd.less";
 import "./i18n";
+import { createGlobalStyle } from "styled-components";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false, retry: false } },
 });
 
+const GlobalStyle = createGlobalStyle`
+  body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Noto Sans KR', sans-serif;
+    };
+`;
+
 ReactDOM.render(
   <RecoilRoot>
     <QueryClientProvider client={queryClient}>
       <ConfigProvider locale={koKR}>
+        <GlobalStyle />
         <Router />
       </ConfigProvider>
     </QueryClientProvider>
   </RecoilRoot>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
