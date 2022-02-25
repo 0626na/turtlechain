192399c739708f098ee2d5c8e9b2cbc15722100a
정보승 <bsjung@turtleship.io>
2022-02-25 16:54:39 +0900

    Update: WarehousingSheetList 컴포넌트 UI 수정

--- src/pages/WarehousingListPage/WarehousingSheetList.tsx
+++ src/pages/WarehousingListPage/WarehousingSheetList.tsx
@@ -3,13 +3,14 @@ import moment from "moment";
 import { useTranslation } from "react-i18next";
 import { RequestGetSheet, WarehousingSheet } from "apis/warehousingAPI";
 import { DeleteFilled, CheckOutlined } from "@ant-design/icons";
-import { Table, Tag, Button, Popconfirm, Pagination } from "antd";
+import { Table, Tag, Button, Popconfirm, Pagination, Row } from "antd";
 import SimplePagination from "components/SimplePagination";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import { storeState } from "store/storeState";
 import { getLocalDateTimeString } from "utils/general";
+import TurtleText from "components/common/TurtleText";
 
 interface Props {
   isLoading: boolean;
@@ -22,7 +23,6 @@ interface Props {
   onDelete: (row: WarehousingSheet) => void;
   onConfirm: (row: WarehousingSheet) => void;
 }
-
 
 const WarehousingSheetList = function ({
   isLoading,
@@ -38,158 +38,156 @@ const WarehousingSheetList = function ({
   const { t } = useTranslation();
   const store = useRecoilValue(storeState);
   const [getSheetQuery, setGetSheetQuery] = useState<RequestGetSheet>({
-    rt_store_id:-1,
+    rt_store_id: -1,
     is_confirmed: "",
-    start_date:"", 
-    end_date:"",
-    page:1
+    start_date: "",
+    end_date: "",
+    page: 1,
   });
 
   let adjTypes = {
-    "exchange" : "교환",
-    "reserve" : "미송",
-    "balance" : "잔",
-    "takeback" : "반품"
-  }
-  useEffect(() => {
+    exchange: "교환",
+    reserve: "미송",
+    balance: "잔",
+    takeback: "반품",
+  };
+  useEffect(() => {}, [store]);
 
-  },[store]); 
-  
   return (
-    <Table
-      size="small"
-      scroll={{ x: "auto", y: 400 }}
-      pagination={false}
-      onRow={(record) => {
-        return {
-          onClick: () => {
-            onSelectRow(record);
+    <Row>
+      <TurtleText>
+        {`${t("warehousing.list")} `}
+        {`(${totalCount.toLocaleString()})`}
+      </TurtleText>
+      <Table
+        size="small"
+        scroll={{ x: "auto", y: 400 }}
+        pagination={false}
+        onRow={(record) => {
+          return {
+            onClick: () => {
+              onSelectRow(record);
+            },
+          };
+        }}
+        loading={isLoading}
+        dataSource={list}
+        rowKey={(record) => record.id}
+        style={{ height: "580px" }}
+        columns={[
+          {
+            width: 100,
+            align: "center",
+            title: t("progress"),
+            dataIndex: "is_confirmed",
+            render: (_, record) => {
+              const { is_confirmed } = record;
+              const color = is_confirmed ? "green" : "red";
+              const text = is_confirmed ? t("confirmed") : t("waiting");
+              return <Tag color={color}>{text}</Tag>;
+            },
           },
-        };
-      }}
-      loading={isLoading}
-      dataSource={list}
-      rowKey={(record) => record.id}
-      columns={[
-        {
-          width: 100,
-          align: "center",
-          title: t("progress"),
-          dataIndex: "is_confirmed",
-          render: (_, record) => {
-            const { is_confirmed } = record;
-            const color = is_confirmed ? "green" : "red";
-            const text = is_confirmed ? t("confirmed") : t("waiting");
-            return <Tag color={color}>{text}</Tag>;
+          {
+            title: t("vendor.name"),
+            dataIndex: "vendor_name",
           },
-        },
-        {
-          title: t("vendor.name"),
-          dataIndex: "vendor_name",
-        },
-        {
-          width: 120,
-          align: "center",
-          title: t("warehousing.date"),
-          dataIndex: "created_time",
-          render: (_, record) =>
-            getLocalDateTimeString(record.created_time)
+          {
+            width: 120,
+            align: "center",
+            title: t("warehousing.date"),
+            dataIndex: "created_time",
+            render: (_, record) => getLocalDateTimeString(record.created_time),
             // moment(record.created_time).format("YYYY-MM-DD"),
-        },
-        {
-          align: "right",
-          title: t("warehousing.total count"),
-          dataIndex: "total_item_count",
-          render: (_, record) => record.total_item_count.toLocaleString(),
-        },
-        {
-          align: "right",
-          title: t("total supply price"),
-          dataIndex: "total_price",
-          render: (_, record) => record.total_price.toLocaleString(),
-        },
-        {
-          width: 300,
-          align: "center",
-          title: "",
-          dataIndex: "action",
-          render: (_, record) => {
-            return (
-              <ActionContainer
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                <Button //
-                  size="small"
-                  shape="round"
-                  onClick={() => {
-                    onSelectRow(record);
+          },
+          {
+            align: "right",
+            title: t("warehousing.total count"),
+            dataIndex: "total_item_count",
+            render: (_, record) => record.total_item_count.toLocaleString(),
+          },
+          {
+            align: "right",
+            title: t("total supply price"),
+            dataIndex: "total_price",
+            render: (_, record) => record.total_price.toLocaleString(),
+          },
+          {
+            width: 300,
+            align: "center",
+            title: "",
+            dataIndex: "action",
+            render: (_, record) => {
+              return (
+                <ActionContainer
+                  onClick={(e) => {
+                    e.stopPropagation();
                   }}
                 >
-                  {t("view details")}
-                </Button>
-                {!record.is_confirmed && (
-                  <>
-                    <Popconfirm
-                      title={t("description.really delete")}
-                      okText={t("yes")}
-                      cancelText={t("no")}
-                      onConfirm={() => {
-                        onDelete(record);
-                      }}
-                    >
-                      <Button
-                        icon={<DeleteFilled />}
-                        danger
-                        type="primary"
-                        size="small"
-                        shape="round"
+                  <Button //
+                    size="small"
+                    shape="round"
+                    onClick={() => {
+                      onSelectRow(record);
+                    }}
+                  >
+                    {t("view details")}
+                  </Button>
+                  {!record.is_confirmed && (
+                    <>
+                      <Popconfirm
+                        title={t("description.really delete")}
+                        okText={t("yes")}
+                        cancelText={t("no")}
+                        onConfirm={() => {
+                          onDelete(record);
+                        }}
                       >
-                        {t("delete")}
-                      </Button>
-                    </Popconfirm>
-                    <Popconfirm
-                      title={t("description.really confirmed")}
-                      okText={t("yes")}
-                      cancelText={t("no")}
-                      onConfirm={() => {
-                        onConfirm(record);
-                      }}
-                    >
-                      <Button //
-                        icon={<CheckOutlined />}
-                        type="primary"
-                        size="small"
-                        shape="round"
+                        <Button
+                          icon={<DeleteFilled />}
+                          danger
+                          type="primary"
+                          size="small"
+                          shape="round"
+                        >
+                          {t("delete")}
+                        </Button>
+                      </Popconfirm>
+                      <Popconfirm
+                        title={t("description.really confirmed")}
+                        okText={t("yes")}
+                        cancelText={t("no")}
+                        onConfirm={() => {
+                          onConfirm(record);
+                        }}
                       >
-                        {t("confirmed")}
-                      </Button>
-                    </Popconfirm>
-                  </>
-                )}
-              </ActionContainer>
-            );
+                        <Button //
+                          icon={<CheckOutlined />}
+                          type="primary"
+                          size="small"
+                          shape="round"
+                        >
+                          {t("confirmed")}
+                        </Button>
+                      </Popconfirm>
+                    </>
+                  )}
+                </ActionContainer>
+              );
+            },
           },
-        },
-      ]}
-      title={() => (
-        <b>
-          {`${t("warehousing.list")} `}
-          {`(${totalCount.toLocaleString()})`}
-        </b>
-      )}
-      footer={() => (
-        <Footer>
-          <Pagination
-            size="small"
-            total={totalCount}
-            showSizeChanger={false}
-            current={getSheetQuery.page}
-          />
-        </Footer>
-      )}
-    />
+        ]}
+        footer={() => (
+          <Footer>
+            <Pagination
+              size="small"
+              total={totalCount}
+              showSizeChanger={false}
+              current={getSheetQuery.page}
+            />
+          </Footer>
+        )}
+      />
+    </Row>
   );
 };
 
