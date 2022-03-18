258f50ee97cd73ffff426a97c57f0ae474940e0a
정보승 <bsjung@turtleship.io>
2022-03-18 17:01:09 +0900

    Fix: 라우터 경로문제 fix

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -25,7 +25,7 @@ const Router = function () {
 
   return (
     <BrowserRouter>
-      {token ? (
+      {sessionStorageToken || localStorageToken ? (
         <MainLayout
           content={
             <Suspense fallback="loading">
