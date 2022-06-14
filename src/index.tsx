04140083fdcb5e3b1bf840b38d160ac43586ff0b
정보승 <bsjung@turtleship.io>
2022-06-14 12:30:36 +0900

    Create: 채널톡 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ConfigProvider, message } from "antd";
-import Router from "router";
 import koKR from "antd/es/locale/ko_KR";
-import GlobalStyle from "GlobalStyle";
 import "moment/locale/ko";
 import "antd/dist/antd.less";
 import "./i18n";
+import Router from "./router";
+import GlobalStyle from "./GlobalStyle";
+import ChannelService from "./ChannelService";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false, retry: false } },
@@ -16,6 +17,10 @@ const queryClient = new QueryClient({
 message.config({
   top: 60,
   maxCount: 1,
+});
+
+ChannelService.boot({
+  pluginKey: "8da7a859-56dc-473a-b576-50a826e5a5db",
 });
 
 ReactDOM.render(
