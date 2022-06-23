08c602d3d58191013f691c9632fb4d0ba4186b8a
정보승 <bsjung@turtleship.io>
2022-06-23 18:28:26 +0900

    Fix: presetAPI 수정

--- src/apis/presetAPI.ts
+++ src/apis/presetAPI.ts
@@ -3,13 +3,13 @@ import { v2Axios } from '.';
 const getBank = async () => {
   const url = `provisioning/preset/bank`;
   const response = await v2Axios.get(url);
-  return response.data.data;
+  return response.data;
 };
 
 const getBuilding = async () => {
   const url = `provisioning/preset/building`;
   const response = await v2Axios.get(url);
-  return response.data.data;
+  return response.data;
 };
 
 const presetAPI = {
