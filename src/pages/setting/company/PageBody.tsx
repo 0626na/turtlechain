249b085b498b9d3c826ac7b48ef80db4e76cb043
gangpyonoh <gpnoh@turtleship.io>
2022-07-21 13:56:49 +0900

    fix: 사업자 세금계산서 이메일 다중필드 적용

--- src/pages/setting/company/PageBody.tsx
+++ src/pages/setting/company/PageBody.tsx
@@ -11,6 +11,7 @@ import {
   Typography,
   Upload,
   Image,
+  Col,
 } from 'antd';
 import retailerCompanyAPI from '@apis/retailerCompanyAPI';
 import { DaumPostcodeModal } from '@components/combine';
@@ -21,20 +22,59 @@ import {
 } from '@components/common';
 import { bizNumPattern } from '@utils/pattern';
 
+import { MinusCircleOutlined } from '@ant-design/icons';
+import { AxiosError } from 'axios';
+
 function PageBody() {
   const [form] = Form.useForm();
   const [isUpdateMode, setIsUpdateMode] = useState(false);
   const [postcodeModalVisible, setPostcodeModalVisible] = useState(false);
 
-  const getQuery = useQuery('getCompany', retailerCompanyAPI.get);
+  const getQuery = useQuery('getCompany', retailerCompanyAPI.get, {
+    onSuccess: (data) => {
+      form.setFieldsValue({
+        biz_license_file: `${data?.biz_license_path}?_=${+new Date()}`,
+      });
+    },
+  });
 
   const updateQuery = useMutation('updateCompany', retailerCompanyAPI.update, {
-    onSuccess: (data) => {
+    onSuccess: () => {
       message.success(t('message.success update'));
+
       setIsUpdateMode(false);
       getQuery.refetch();
     },
   });
+
+  const checkEmailValidityQuery = useMutation(
+    'checkEmailValidityQuery',
+    retailerCompanyAPI.checkEmailValidity,
+    {
+      onSuccess: (data) => {
+        message.success(data.msg);
+
+        form.setFieldsValue({
+          ...form.getFieldsValue(),
+          email: [
+            ...form.getFieldValue('email'),
+            form.getFieldValue('newEmail'),
+          ],
+        });
+        form.resetFields(['newEmail']);
+      },
+      onError: (data: AxiosError) => {
+        message.error(data.response?.data.msg);
+      },
+    },
+  );
+
+  const normFile = (e: any) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  };
 
   return (
     <Row>
@@ -46,6 +86,12 @@ function PageBody() {
             <Typography.Link
               type="secondary"
               onClick={() => {
+                form.setFieldsValue({
+                  biz_license_file: `${
+                    getQuery?.data?.biz_license_path
+                  }?_=${+new Date()}`,
+                });
+
                 setIsUpdateMode(false);
               }}
             >
@@ -65,6 +111,16 @@ function PageBody() {
                   address_sub: getQuery.data?.address.split('::')[1],
                   email: getQuery.data?.email,
                   memo: getQuery.data?.memo,
+                  biz_license_file: [
+                    {
+                      uid: '1',
+                      name: getQuery.data?.biz_license_path.split('/').pop()!,
+                      status: 'done',
+                      url: `${
+                        getQuery.data?.biz_license_path
+                      }?_=${+new Date()}`,
+                    },
+                  ],
                 });
               }}
             >
@@ -78,14 +134,18 @@ function PageBody() {
           form={form}
           colon={false}
           labelCol={{ span: 4 }}
-          wrapperCol={{ span: 7 }}
+          wrapperCol={{ span: 8 }}
           onFinish={(value) => {
             updateQuery.mutate({
-              ...value,
-              biz_license_file:
-                form.getFieldValue('biz_license_file')?.fileList[0]
-                  .originFileObj,
+              company_id: value.company_id,
+              biz_type: value.biz_type,
+              name: value.name,
+              address_main: value.address_main,
               address_sub: form.getFieldValue('address_sub') ?? '',
+              email: value.email.join(',') ?? '',
+              memo: value.memo,
+              biz_license_file:
+                form.getFieldValue('biz_license_file')[0].originFileObj,
             });
           }}
         >
@@ -93,7 +153,11 @@ function PageBody() {
             <Input hidden />
           </Form.Item>
 
-          <Form.Item label={t('company.type.')} name="biz_type">
+          <Form.Item
+            label={t('company.type.')}
+            name="biz_type"
+            wrapperCol={{ span: 6 }}
+          >
             {isUpdateMode ? (
               <Select>
                 <Select.Option value="personal">
@@ -113,24 +177,31 @@ function PageBody() {
           <Form.Item label="사업자 번호">
             {getQuery.data?.biz_num.replace(bizNumPattern, '$1-$2-$3')}
           </Form.Item>
-          <Form.Item label="사업자명" name="name">
+          <Form.Item label="사업자명" name="name" wrapperCol={{ span: 6 }}>
             {isUpdateMode ? <Input /> : getQuery.data?.name}
           </Form.Item>
-          <Form.Item label="사업자 주소" name="address_main">
+
+          <Form.Item label="사업자 주소">
             {isUpdateMode ? (
-              <Input
-                readOnly
-                suffix={
+              <Row>
+                <Col span={18}>
+                  <Form.Item noStyle name="address_main">
+                    <Input
+                      readOnly
+                      onClick={() => setPostcodeModalVisible(true)}
+                    />
+                  </Form.Item>
+                </Col>
+                <Col>
                   <Button
-                    size="small"
-                    type="link"
-                    style={{ fontSize: 13 }}
+                    style={{ marginLeft: 8 }}
+                    size="middle"
                     onClick={() => setPostcodeModalVisible(true)}
                   >
                     {t('find address')}
                   </Button>
-                }
-              />
+                </Col>
+              </Row>
             ) : (
               getQuery.data?.address
             )}
@@ -142,46 +213,122 @@ function PageBody() {
               form.setFieldsValue({ ...form.getFieldsValue, address_main });
             }}
           />
-          {isUpdateMode && (
-            <Form.Item label="상세주소" name="address_sub">
-              <Input />
-            </Form.Item>
-          )}
-          <Form.Item label="세금계산서 발행메일" name="email">
-            {isUpdateMode ? <Input /> : getQuery.data?.email}
+
+          <Form.Item
+            label="상세주소"
+            name="address_sub"
+            wrapperCol={{ span: 6 }}
+            hidden={!isUpdateMode}
+          >
+            <Input />
           </Form.Item>
-          <Form.Item label="메모" name="memo">
+
+          <Form.Item label="세금계산서 발행메일">
+            <Form.Item
+              style={{
+                marginBottom: 8,
+                display: isUpdateMode ? 'block' : 'none',
+              }}
+            >
+              <Row>
+                <Col span={18}>
+                  <Form.Item noStyle name="newEmail">
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col>
+                  <Form.Item shouldUpdate noStyle>
+                    {({ getFieldValue }) => (
+                      <Button
+                        style={{ marginLeft: 8 }}
+                        disabled={getFieldValue('email')?.length === 4}
+                        loading={checkEmailValidityQuery.isLoading}
+                        onClick={() => {
+                          checkEmailValidityQuery.mutate({
+                            email: getFieldValue('newEmail'),
+                          });
+                        }}
+                      >
+                        추가
+                      </Button>
+                    )}
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Form.Item>
+
+            <Form.List name="email">
+              {(fields, { remove }) => {
+                if (!isUpdateMode) {
+                  return (
+                    <>
+                      {getQuery.data?.email.map((item, idx) => {
+                        return <div key={idx}>{item}</div>;
+                      })}
+                    </>
+                  );
+                }
+
+                return (
+                  <>
+                    {fields.map((field) => {
+                      return (
+                        <Row
+                          style={{ marginBottom: 8 }}
+                          key={field.key}
+                          align="middle"
+                          gutter={12}
+                        >
+                          <Col span={12}>
+                            <Form.Item {...field} noStyle>
+                              <Input disabled />
+                            </Form.Item>
+                          </Col>
+                          <Col span={3}>
+                            <MinusCircleOutlined
+                              style={{ padding: 8 }}
+                              onClick={() => remove(field.name)}
+                            />
+                          </Col>
+                        </Row>
+                      );
+                    })}
+                  </>
+                );
+              }}
+            </Form.List>
+          </Form.Item>
+
+          <Form.Item label="메모" name="memo" wrapperCol={{ span: 6 }}>
             {isUpdateMode ? <Input /> : getQuery.data?.memo}
           </Form.Item>
-          <Form.Item label="사업자등록증" name="biz_license_file">
+          <Form.Item
+            label="사업자등록증"
+            name="biz_license_file"
+            wrapperCol={{ span: 6 }}
+            getValueFromEvent={isUpdateMode ? normFile : () => {}}
+            valuePropName={isUpdateMode ? 'fileList' : 'src'}
+          >
             {isUpdateMode ? (
               <Upload
                 listType="picture"
                 maxCount={1}
                 accept=".jpg, .png, .jpeg, .pdf"
                 beforeUpload={() => false}
-                onRemove={() => false}
-                defaultFileList={[
-                  {
-                    uid: '1',
-                    name: getQuery.data?.biz_license_path.split('/').pop()!,
-                    status: 'done',
-                    url: getQuery.data?.biz_license_path,
-                  },
-                ]}
               >
                 <TurtleButtonSub size="small">파일 선택하기</TurtleButtonSub>
               </Upload>
             ) : (
-              <Image
-                width={300}
-                src={`${getQuery.data?.biz_license_path}?_=${+new Date()}`}
-              />
+              <Image width={300} />
             )}
           </Form.Item>
           <Row justify="end">
             {isUpdateMode && (
-              <TurtleButton htmlType="submit" type="default">
+              <TurtleButton
+                htmlType="submit"
+                type="default"
+                loading={updateQuery.isLoading}
+              >
                 수정하기
               </TurtleButton>
             )}
