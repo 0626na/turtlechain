692b6ecc5f8d114e423547671d78f8f092e43bf5
gangpyonoh <gpnoh@turtleship.io>
2022-08-01 14:24:08 +0900

    fix: query key 수정

--- src/pages/clearing/balance/DetailModal.tsx
+++ src/pages/clearing/balance/DetailModal.tsx
@@ -21,7 +21,7 @@ function DetailModal({ visible, closeModal, selectedRow }: Props) {
   const store = useRecoilValue(storeState);
 
   const getRetailerStoreOverpaidBalanceQuery = useQuery(
-    ['getRetailerStoreOverpaidBalanceQuery', selectedRow],
+    ['getRetailerStoreOverpaidBalance', selectedRow],
     () =>
       clearingAPI.getOverpaidBalance({
         rt_store_id: store.id!,
