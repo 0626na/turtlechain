7fc0dcf5fa741307ccda5b6d2006410c7d7e4aa1
sukjin <sjhan@turtleship.io>
2022-08-15 14:39:27 +0900

    fix: 상품등록시 셀메이트와 데이터가 다를시에 다른곳을 표시하는 기능의 브라우저 console에 의 warning 출력 수정

--- src/pages/product/create/SuccessTab.tsx
+++ src/pages/product/create/SuccessTab.tsx
@@ -26,9 +26,7 @@ function SuccessTab({ loading, ...props }: Props) {
   const [messageVisible, setMessageVisible] = useState(false);
 
   const needUpdateStyle = (needUpdate: boolean) => ({
-    style: {
-      backgroundColor: needUpdate ? '#F2F2F3' : 'transparent',
-    },
+    backgroundColor: needUpdate ? '#F2F2F3' : 'transparent',
   });
 
   // 상품 삭제
@@ -84,19 +82,19 @@ function SuccessTab({ loading, ...props }: Props) {
             ellipsis: true,
             width: 150,
             title: t('vendor.name'),
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: record.vendor_name,
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => record.vendor_name,
           },
           {
             ellipsis: true,
             width: 150,
             title: t('vendor.address'),
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: record.vendor_address,
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => record.vendor_address,
           },
           {
             ellipsis: true,
@@ -130,95 +128,95 @@ function SuccessTab({ loading, ...props }: Props) {
                 {t('product.name')}
               </Popover>
             ),
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: record.name,
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => record.name,
           },
           {
             ellipsis: true,
             width: 200,
             title: t('product.vendor product name'),
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: record.vendor_product_name,
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => record.vendor_product_name,
           },
           {
             ellipsis: true,
             width: 150,
             title: t('product.code'),
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: record.product_code,
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => record.product_code,
           },
           {
             ellipsis: true,
             width: 150,
             title: t('product.option'),
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: record.option,
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => record.option,
           },
           {
             ellipsis: true,
             align: 'right',
             width: 120,
             title: t('product.price'),
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: (
-                <TurtleInputPrice
-                  size="small"
-                  value={record.price}
-                  onChange={(value) => {
-                    updateSuccessList('price', record.product_code, value);
-                    updateSuccessList(
-                      'need_update',
-                      record.product_code,
-                      record.submit_price !== value,
-                    );
-                  }}
-                />
-              ),
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => (
+              <TurtleInputPrice
+                size="small"
+                value={record.price}
+                onChange={(value) => {
+                  updateSuccessList('price', record.product_code, value);
+                  updateSuccessList(
+                    'need_update',
+                    record.product_code,
+                    record.submit_price !== value,
+                  );
+                }}
+              />
+            ),
           },
           {
             ellipsis: true,
             title: t('product.image url'),
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: (
-                <Typography.Link href={record.image_url} target="_blank">
-                  {record.image_url}
-                </Typography.Link>
-              ),
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => (
+              <Typography.Link href={record.image_url} target="_blank">
+                {record.image_url}
+              </Typography.Link>
+            ),
           },
           {
             ellipsis: true,
             title: t('product.memo'),
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: record.memo,
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => record.memo,
           },
           {
             ellipsis: true,
             width: 50,
-            render: (_, record) => ({
-              props: needUpdateStyle(record.need_update),
-              children: (
-                <TurtleIcon
-                  type="delete"
-                  onClick={() => {
-                    deleteItem(record.product_code);
-                  }}
-                />
-              ),
+            onCell: (record) => ({
+              style: needUpdateStyle(record.need_update),
             }),
+            render: (_, record) => (
+              <TurtleIcon
+                type="delete"
+                onClick={() => {
+                  deleteItem(record.product_code);
+                }}
+              />
+            ),
           },
         ]}
       />
