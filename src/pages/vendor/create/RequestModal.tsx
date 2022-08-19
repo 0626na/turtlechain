4841e5955be64641aa019f2071cb027dc7d30c30
정보승 <bsjung@turtleship.io>
2022-08-19 16:58:21 +0900

    Update: 거래처 신규/수정 요청 층, 열/호 필수값 제거

--- src/pages/vendor/create/RequestModal.tsx
+++ src/pages/vendor/create/RequestModal.tsx
@@ -126,12 +126,7 @@ function RequestModal({ visible, closeModal }: Props) {
                 )}
               </Select>
             </Form.Item>
-            <Form.Item
-              name="floor"
-              label="층"
-              noStyle
-              rules={[{ required: true }]}
-            >
+            <Form.Item name="floor" label="층" noStyle>
               <Select
                 placeholder="층"
                 style={{ width: '25%' }}
@@ -152,12 +147,7 @@ function RequestModal({ visible, closeModal }: Props) {
                 ))}
               </Select>
             </Form.Item>
-            <Form.Item
-              name="colLoc"
-              noStyle
-              label="열/호"
-              rules={[{ required: true }]}
-            >
+            <Form.Item name="colLoc" noStyle label="열/호">
               <Select placeholder="열/호" style={{ width: '35%' }}>
                 {(
                   getBuildingQuery.data?.data[address.building]?.[
