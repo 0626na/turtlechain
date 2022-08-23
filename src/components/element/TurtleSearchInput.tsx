f7ca53ccd0bbba1b80ad5becde38d4b489d8e984
gangpyonoh <gpnoh@turtleship.io>
2022-08-23 14:17:10 +0900

    update : turtleSearchInput

---
+++ src/components/element/TurtleSearchInput.tsx
@@ -0,0 +1,66 @@
+import { Form } from 'antd';
+import { Input } from 'antd';
+import styled from 'styled-components';
+
+const { Search } = Input;
+
+interface Props {
+  name?: string;
+  value?: string;
+  label?: string;
+  placeholder?: string;
+  onClick?: () => void;
+}
+
+function TurtleSearchInput({
+  name,
+  value,
+  label,
+  placeholder,
+  onClick,
+}: Props) {
+  return (
+    <StyledSearch
+      style={{ width: 220 }}
+      placeholder={placeholder}
+      value={value}
+      onClick={onClick}
+      onSearch={onClick}
+    />
+  );
+}
+
+const StyledSearch = styled(Search)`
+  // 검색 인풋 style
+  input {
+    height: 28px;
+    background: #f5f6f7;
+    padding: 7.5px 10px;
+
+    color: #242934;
+    font-size: 13px;
+
+    border: none;
+    border-radius: 6px 0px 0px 6px;
+  }
+
+  // 검색 아이콘 버튼 style
+  &.ant-input-search
+    > .ant-input-group
+    > .ant-input-group-addon:last-child
+    .ant-input-search-button,
+  .ant-input-group-addon {
+    border-radius: 0px 6px 6px 0px;
+  }
+
+  .ant-input-search-button {
+    background: #f5f6f7;
+    height: 28px;
+  }
+
+  svg {
+    color: #242934;
+  }
+`;
+
+export default TurtleSearchInput;
