566b1c20b9d1a5f8084c5b6f85b95929c153f1d7
gangpyonoh <gpnoh@turtleship.io>
2022-08-26 14:20:07 +0900

    update: App 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,141 @@ function App() {
   return (
     <Suspense fallback={<></>}>
       <Routes>
-        <Route element={<MainLayout />}>
+        {/* <Route index element={<LoginPage />} />
+        <Route path="/find-id" element={<FindIdPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/membership-info" element={<MembershipInfoPage />} />
+        <Route path="*" element={<NotFoundPage />} /> */}
+
+        <Route path="/" element={<MainLayout />}>
+          {/*
+           * 메인
+           */}
+
           <Route
-            path="/"
+            index
             element={
               <Suspense fallback={<></>}>
                 <HomePage />
               </Suspense>
             }
           />
+
+          {/*
+           * 거래처/상품
+           */}
+
+          {/* 
+          <Route
+            path="vendor/create"
+            element={
+              <Suspense fallback={<></>}>
+                <VendorCreatePage />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="product/create"
+            element={
+              <Suspense fallback={<></>}>
+                <ProductCreatePage />
+              </Suspense>
+            }
+          /> 
+          */}
+
+          {/*
+           * 발주
+           */}
+
+          {/* <Route
+            path="order/create"
+            element={
+              <Suspense fallback={<></>}>
+                <OrderCreatePage />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="order/history"
+            element={
+              <Suspense fallback={<></>}>
+                <OrderHistoryPage />
+              </Suspense>
+            }
+          /> */}
+
+          {/*
+           * 입고
+           */}
+
+          {/* <Route
+            path="warehousing/create"
+            element={
+              <Suspense fallback={<></>}>
+                <WarehousingCreatePage />
+              </Suspense>
+            }
+          />
+        </Route> 
+
+          <Route
+            path="warehousing/history"
+            element={
+              <Suspense fallback={<></>}>
+                <WarehousingHistoryPage />
+              </Suspense>
+            }
+          />
+        </Route> 
+
+          <Route
+            path="warehousing/adjustment"
+            element={
+              <Suspense fallback={<></>}>
+                <WarehousingCreatePage />
+              </Suspense>
+            }
+          />
+        </Route> */}
+
+          {/*
+           * 결제
+           */}
+
+          {/* <Route
+            path="clearing/create"
+            element={
+              <Suspense fallback={<></>}>
+                <ClearingCreatePage />
+              </Suspense>
+            }
+          />
+        </Route> 
+
+          <Route
+            path="clearing/history"
+            element={
+              <Suspense fallback={<></>}>
+                <ClearingHistoryPage />
+              </Suspense>
+            }
+          />
+        </Route> 
+
+          <Route
+            path="clearing/trade"
+            element={
+              <Suspense fallback={<></>}>
+                <ClearingTradePage />
+              </Suspense>
+            }
+          />
+        </Route> */}
         </Route>
       </Routes>
     </Suspense>
