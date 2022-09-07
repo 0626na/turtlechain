e9de0fdab30fe4264718701295b17e46513c2612
gangpyono <shrkdvy123@gmail.com>
2022-09-08 01:40:35 +0900

    update : 불필요 코드 삭제

--- src/components/combine/index.tsx
+++ src/components/combine/index.tsx
@@ -1,6 +1,6 @@
 import Notification from './Notification';
 import TurtleAnswerModal from './TurtleAnswerModal';
-import TurtleModaltest from './TurtleModaltest';
+
 import TurtleContentModal from './TurtleContentModal';
 
-export { Notification, TurtleAnswerModal, TurtleContentModal, TurtleModaltest };
+export { Notification, TurtleAnswerModal, TurtleContentModal };
