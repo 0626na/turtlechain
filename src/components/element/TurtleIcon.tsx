82d21bbada6d4bb0a51ae2e86db90f8854a05ddc
gangpyonoh <gpnoh@turtleship.io>
2022-09-08 15:25:04 +0900

    update : modalClose 아이콘 추가

--- src/components/element/TurtleIcon.tsx
+++ src/components/element/TurtleIcon.tsx
@@ -1,30 +1,50 @@
 import { ReactComponent as RemoveIcon } from '@icons/remove.svg';
 import { ReactComponent as MemoIcon } from '@icons/memo.svg';
+import { ReactComponent as ModalCloseIcon } from '@icons/modalClose.svg';
 import { css } from '@emotion/react';
 
 interface Props {
-  name: 'delete' | 'memo';
+  name: 'delete' | 'memo' | 'modalClose';
   onClick?: () => void;
+  isPadding?: boolean;
 }
 
-function TurtleIcon({ name, onClick }: Props) {
+function TurtleIcon({ name, onClick, isPadding }: Props) {
   const handleClick = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
     e.stopPropagation();
     onClick && onClick();
   };
 
   if (name === 'delete') {
-    return <RemoveIcon css={icon} onClick={handleClick} />;
-  } else if ((name = 'memo')) {
-    return <MemoIcon css={icon} onClick={handleClick} />;
-  } else {
-    return <></>;
+    return (
+      <RemoveIcon css={[icon, isPadding && padding]} onClick={handleClick} />
+    );
   }
+
+  if (name === 'memo') {
+    return (
+      <MemoIcon css={[icon, isPadding && padding]} onClick={handleClick} />
+    );
+  }
+
+  if (name === 'modalClose') {
+    return (
+      <ModalCloseIcon
+        css={[icon, isPadding && padding]}
+        onClick={handleClick}
+      />
+    );
+  }
+
+  return <></>;
 }
 
 const icon = css`
   cursor: pointer;
-  padding: 6;
+`;
+
+const padding = css`
+  padding: 6px;
 `;
 
 export default TurtleIcon;
