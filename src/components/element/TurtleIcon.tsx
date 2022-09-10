5aae7a556b457ae4589e81c631a4a09af58b9652
gangpyono <shrkdvy123@gmail.com>
2022-09-10 13:46:05 +0900

    update : exel,single(엑셀,단건추가 아이콘추가)

--- src/components/element/TurtleIcon.tsx
+++ src/components/element/TurtleIcon.tsx
@@ -4,10 +4,20 @@ import { ReactComponent as ModalCloseIcon } from '@icons/modalClose.svg';
 
 import { ReactComponent as MatchingIcon } from '@icons/matching.svg';
 import { ReactComponent as MisMatchingIcon } from '@icons/misMatching.svg';
+
+import { ReactComponent as ExelIcon } from '@icons/exel.svg';
+import { ReactComponent as SingleIcon } from '@icons/single.svg';
 import { css } from '@emotion/react';
 
 interface Props {
-  name: 'delete' | 'memo' | 'modalClose' | 'matching' | 'misMatching';
+  name:
+    | 'delete'
+    | 'memo'
+    | 'modalClose'
+    | 'matching'
+    | 'misMatching'
+    | 'exel'
+    | 'single';
   onClick?: () => void;
   className?: string;
 }
@@ -54,6 +64,22 @@ function TurtleIcon({ name, onClick, className }: Props) {
     return (
       <div css={iconContainer} onClick={handleClick}>
         <MisMatchingIcon className={className} />
+      </div>
+    );
+  }
+
+  if (name === 'exel') {
+    return (
+      <div css={iconContainer} onClick={handleClick}>
+        <ExelIcon className={className} />
+      </div>
+    );
+  }
+
+  if (name === 'single') {
+    return (
+      <div css={iconContainer} onClick={handleClick}>
+        <SingleIcon className={className} />
       </div>
     );
   }
