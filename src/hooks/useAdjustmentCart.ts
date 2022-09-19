2d33390af9df823268f681f0c1edf6eae0ad3a96
gangpyonoh <gpnoh@turtleship.io>
2022-09-19 22:55:41 +0900

    update : 불필요 코드 제거

--- src/hooks/useAdjustmentCart.ts
+++ src/hooks/useAdjustmentCart.ts
@@ -4,12 +4,9 @@ import { useRecoilState } from 'recoil';
 const useAdjustmentCart = () => {
   const [cart, setCart] = useRecoilState(adjustmentCartState);
 
-  const passingToExchangeRefundPanel = () => {}; // 입고판넬 -> 교환/환불 판넬로 이동
-
   return {
     cart,
     setCart,
-    passingToExchangeRefundPanel,
   };
 };
 
