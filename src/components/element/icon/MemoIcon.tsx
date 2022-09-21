0bcb691f5ef32fadf332db9cd1019ffb39b2c509
gangpyonoh <gpnoh@turtleship.io>
2022-09-21 11:25:43 +0900

    update : 이벤트 인자 적용

--- src/components/element/icon/MemoIcon.tsx
+++ src/components/element/icon/MemoIcon.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ReactComponent as Memo } from '@icons/memo.svg';
 
 interface Props {
+  // 사용처에서 이벤트전파 제어를 하기위해 인자 타입을 지정해준다.
   onClick?: (e: any) => void;
   value: string;
 }
