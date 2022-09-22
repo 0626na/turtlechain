7dfebbaf7a8b70feb8ba814a6e6a629248231ac5
정보승 <bsjung@turtleship.io>
2022-09-22 15:47:43 +0900

    Feat: home 페이지 정리

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { t } from 'i18next';
 import { Helmet } from 'react-helmet';
 
-import { AlertBar } from '@layout/page';
+import { PageHeader } from '@layout/page';
 import PageBody from './PageBody';
 
 function index() {
   return (
     <>
       <Helmet title={`${t('helmet.turtleChain')} - ${t('helmet.home')}`} />
-      <AlertBar />
+      <PageHeader title="" />
       <PageBody />
     </>
   );
