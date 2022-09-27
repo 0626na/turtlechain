4aa230262d275a972d1a1ef56725580914cc06f3
gangpyonoh <gpnoh@turtleship.io>
2022-09-27 15:53:32 +0900

    feat : 불필요코드 삭제

--- src/pages/setting/PageBody.tsx
+++ src/pages/setting/PageBody.tsx
@@ -1,81 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import {
-  SecondaryButton,
-  TurtleFormInput,
-  TurtleIcon,
-  TurtleSearchInput,
-  TurtleTableTitle,
-  TurtleTabs,
-} from '@components/element';
-import useModal from '@hooks/useModal';
-import useStore from '@hooks/useStore';
-import { PageContent, PageHeader, PageTitle } from '@layout/page';
-import { Form, Pagination, Radio, Row, Select, Steps, Table, Tabs } from 'antd';
+import { TurtleFormInput, TurtleIcon, TurtleTabs } from '@components/element';
+
+import { PageHeader } from '@layout/page';
+import { Form, Radio, Tabs } from 'antd';
 import { t } from 'i18next';
-import { useQuery } from 'react-query';
 
-import clearingAPI, {
-  ClearingInfo,
-  RequestGetOverpaidBalanceList,
-} from '@apis/clearingAPI';
-// import DetailModal from './modals/DetailModal';
-import moment from 'moment';
 import { css } from '@emotion/react';
-import { FileMarkdownFilled } from '@ant-design/icons';
+
 import Card from './Card';
 import UserTab from './tabs/UserTab';
-// import AddModal from './modals/AddModal';
 
 function PageBody() {
-  const [selectedRow, setSelectedRow] = useState<ClearingInfo>();
-  const { store } = useStore();
-  const [searchQuery, setSearchQuery] = useState<RequestGetOverpaidBalanceList>(
-    {
-      rt_store_id: undefined,
-      balance_type: 'balance',
-      search_string: '',
-      page: 1,
-    },
-  );
-
-  const [addModalVisible, addModalOpen, addModalClose] = useModal();
-  const [detailModalVisible, detailModalOpen, detailModalClose] = useModal();
-
-  // 장부 리스트 불러오기 요청
-  const getOverpaidBalanceListQuery = useQuery(
-    ['getOverpaidBalanceList', searchQuery],
-    () => clearingAPI.getOverpaidBalanceList(searchQuery),
-    {
-      enabled: !!searchQuery.rt_store_id,
-    },
-  );
-
-  const loading = getOverpaidBalanceListQuery.isLoading;
-
-  // 쇼핑몰 바뀔때 상품 리스트 재검색
-  useEffect(() => {
-    setSearchQuery((searchQuery) => ({
-      ...searchQuery,
-      rt_store_id: store.selected?.id as number,
-    }));
-  }, [store.selected?.id]);
-
   return (
     <>
-      {/*
-       * 과거매입 모달
-       */}
-      {/* <AddModal visible={addModalVisible} closeModal={addModalClose} /> */}
-      {/*
-       * 상세보기 모달
-       */}
-      {/* <DetailModal
-        selectedRow={selectedRow as ClearingInfo}
-        visible={detailModalVisible}
-        onClose={detailModalClose}
-      /> */}
-
       <PageHeader title="설정" />
 
       <div // pageContent
