8ddf47646ee0a7ec3505a3b9c9dee8fcbb8c2e68
gangpyonoh <gpnoh@turtleship.io>
2022-09-28 01:44:23 +0900

    feat : 장부 interface 설정

--- src/apis/transactionAPI.ts
+++ src/apis/transactionAPI.ts
@@ -1,53 +1,22 @@
 import { v2Axios } from '.';
 
-// 정산서
-export interface ClearingSheetShow {
-  id: number;
-  created_time: string;
-  status: 'request' | 'pending' | 'complete';
-  store_id: number;
-  store_name: string;
-  request_date: string;
-  complete_date: string | null;
-  total_clearing_amount: number;
-  total_deposit_amount: number;
-  included_vat_amount: number;
+export interface TransactionDetailItem {
+  id?: number;
+  created_date: string;
+  transaction_type: string;
+  memo: string;
+  unpaid_amount: number;
+  subtract_amount: number;
+  refund_amount: number;
 }
 
-// 정산 아이템
-export interface ClearingItemShow {
-  id: number;
-  created_by: number;
-  created_time: string;
-  created_date: string;
-  sheet_id: number;
-  is_inactive: boolean;
-  type: string;
-  original_id: number;
-  adjustment_type: string;
-  adjustment_process_type: string;
-  rt_store_id: number;
-  rt_store_name: string;
+export interface TransactionItem {
   vendor_id: number;
   vendor_name: string;
   vendor_address: string;
-  recipient_print: string;
-  is_vat_included: boolean;
-  bank: string;
-  account_number: string;
-  account_holder: string;
-  memo: string | null;
-  total_amount: number;
-  clearing_amount: number;
-  supply_amount: number;
-  vat_amount: number;
-  complete_date: string;
-  ws_store_id: {
-    id: number;
-    store_phone: Array<{
-      phone: string;
-    }>;
-  };
+  unpaid_amount: number; // 결제요청금액
+  subtract_amount: number; // 사용가능금액
+  refund_amount: number; // 환불 예정금액
 }
 
 /*
@@ -55,18 +24,12 @@ export interface ClearingItemShow {
  */
 
 export interface RequestGetList {
-  rt_store_id: number;
+  rt_store_id: number | undefined;
 }
 
 export interface ResponseGetList {
   msg: string;
-  data: {
-    vendor_name: string;
-    vendor_address: string;
-    unpaid_amount: number; // 결제요청금액
-    subtract_amount: number; // 사용가능금액
-    refund_amount: number; // 환불 예정금액
-  };
+  data: TransactionItem[];
 }
 
 const getList = async (params: RequestGetList) => {
@@ -78,22 +41,15 @@ const getList = async (params: RequestGetList) => {
 
 //거래장부 상세 조회
 
-interface RequestGetItem {
-  vendor_id: string;
+export interface RequestGetItem {
+  vendor_id: number | undefined;
   start_date: string; // YYYY-MM-DD
   end_date: string; // // YYYY-MM-DD
 }
 
 interface ResponseGetItem {
   msg: string;
-  data: {
-    created_date: string;
-    transaction_type: string; //
-    memo: string;
-    unpaid_amount: number;
-    subtract_amount: number;
-    refund_amount: number;
-  }[];
+  data: TransactionDetailItem[];
 }
 
 const getItem = async (params: RequestGetItem) => {
