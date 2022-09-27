bb966517bd7cef426d888fdee76baf622e942553
gangpyonoh <gpnoh@turtleship.io>
2022-09-27 15:44:31 +0900

    feat : 사이더 이미지 설정

--- src/layout/retailerMain/StoreSelector.tsx
+++ src/layout/retailerMain/StoreSelector.tsx
@@ -1,6 +1,6 @@
 import { Button, Dropdown, Menu } from 'antd';
 import { useQuery } from 'react-query';
-import { ArrowRightIcon, TurtleImg } from '@components/element';
+import { ArrowRightIcon, TurtleIcon, TurtleImg } from '@components/element';
 import { css } from '@emotion/react';
 import retailerStoreAPI from '@apis/retailerStoreAPI';
 import useStore from '@hooks/useStore';
@@ -55,7 +55,7 @@ function StoreSelector() {
           <span css={text}>{store.selected?.name}</span>
         </div>
         <div>
-          <ArrowRightIcon value="#AAADB3" />
+          <TurtleIcon name="siderSelect" />
         </div>
       </Button>
     </Dropdown>
