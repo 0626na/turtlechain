08e0b8c555cf5323f84541f8cf571e5c792b38e2
gangpyonoh <gpnoh@turtleship.io>
2022-09-28 12:59:44 +0900

    feat : api함수명 변경

--- src/apis/transactionAPI.ts
+++ src/apis/transactionAPI.ts
@@ -73,7 +73,7 @@ interface ResponseCreateItem {
   data: null;
 }
 
-const createItem = async (data: RequestCreateItem) => {
+const create = async (data: RequestCreateItem) => {
   const url = `accounting/transactions`;
   const response = await v2Axios.post<ResponseCreateItem>(url, { data });
 
@@ -83,7 +83,7 @@ const createItem = async (data: RequestCreateItem) => {
 const transactionAPI = {
   getList,
   getItem,
-  createItem,
+  create,
 };
 
 export default transactionAPI;
