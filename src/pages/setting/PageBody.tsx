9a9cbc7c3146283fe8ea7ac1ac7502374ef1bcf7
gangpyonoh <gpnoh@turtleship.io>
2022-09-29 10:13:05 +0900

    feat : 사업자정보 컴포넌트 분리

--- src/pages/setting/PageBody.tsx
+++ src/pages/setting/PageBody.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
 
-import { TurtleFormInput, TurtleIcon, TurtleTabs } from '@components/element';
+import { TurtleTabs } from '@components/element';
 
 import { PageHeader } from '@layout/page';
-import { Form, Radio, Tabs } from 'antd';
-import { t } from 'i18next';
+import { Tabs } from 'antd';
 
 import { css } from '@emotion/react';
 
-import Card from './Card';
 import UserTab from './tabs/UserTab';
 import { useSearchParams } from 'react-router-dom';
+import CompanyTab from './tabs/CompanyTab';
+import StoreTab from './tabs/StoreTab';
 
 function PageBody() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -25,10 +25,11 @@ function PageBody() {
       <PageHeader title="설정" />
 
       <div // pageContent
-        css={css`
-          flex-grow: 1;
-          background-color: #f9f9fa;
-        `}
+        css={{
+          flexGrow: 1,
+          backgroundColor:
+            searchParams.get('tab') === 'store' ? '#fff' : '#f9f9fa',
+        }}
       >
         <TurtleTabs
           color="dark"
@@ -38,7 +39,9 @@ function PageBody() {
           }}
         >
           <Tabs.TabPane key="store" tab="쇼핑몰 관리">
-            <div css={tabContent}>ㅁ</div>
+            <div css={[storeTabContent]}>
+              <StoreTab />
+            </div>
           </Tabs.TabPane>
           <Tabs.TabPane key="user" tab="계정관리">
             <div css={tabContent}>
@@ -47,35 +50,7 @@ function PageBody() {
           </Tabs.TabPane>
           <Tabs.TabPane key="company" tab="사업자 관리">
             <div css={tabContent}>
-              <Card title="사업자 정보" icon={<TurtleIcon name="user" />}>
-                <Form
-                  onValuesChange={(changedValues, allValues) => {
-                    console.log(changedValues, allValues); //  수정버튼 활성화.
-                  }}
-                  colon={false}
-                  labelCol={{ span: 7 }}
-                  wrapperCol={{ span: 17 }}
-                >
-                  <Form.Item label="사업자 종류" name="aaa">
-                    <Radio.Group>
-                      {['entity', 'personal', 'simple'].map((option) => (
-                        <Radio key={option} value={option}>
-                          {t(`biz ${option}`)}
-                        </Radio>
-                      ))}
-                    </Radio.Group>
-                  </Form.Item>
-                  <Form.Item label="사업자명(법인명)">
-                    <TurtleFormInput />
-                  </Form.Item>
-                  <Form.Item label="이메일">
-                    <TurtleFormInput />
-                  </Form.Item>
-                  <Form.Item label="휴대전화 번호">
-                    <TurtleFormInput />
-                  </Form.Item>
-                </Form>
-              </Card>
+              <CompanyTab />
             </div>
           </Tabs.TabPane>
 
@@ -90,6 +65,10 @@ function PageBody() {
 
 const tabContent = css`
   padding: 38px 36px 0px 36px;
+`;
+
+const storeTabContent = css`
+  padding: 30px 36px 0px 36px;
 `;
 
 export default PageBody;
