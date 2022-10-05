eb1ab8d0a8c413c012b431dbe7ef32b42b2904a7
gangpyonoh <gpnoh@turtleship.io>
2022-10-05 11:10:17 +0900

    feat : 아이디찾기 레이아웃 추가

--- src/layout/login/index.tsx
+++ src/layout/login/index.tsx
@@ -1,4 +1,5 @@
+import FindIdPageBody from './FindIdPageBody';
 import LoginPageBody from './LoginPageBody';
 import SignupPageBody from './SignupPageBody';
 
-export { LoginPageBody, SignupPageBody };
+export { LoginPageBody, SignupPageBody, FindIdPageBody };
