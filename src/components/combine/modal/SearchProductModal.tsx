8481c22c9f0beca3f90d7da73ef2e312a24a5f4d
gangpyonoh <gpnoh@turtleship.io>
2022-10-06 14:24:22 +0900

    feat : z-index수정

--- src/components/combine/modal/SearchProductModal.tsx
+++ src/components/combine/modal/SearchProductModal.tsx
@@ -56,7 +56,7 @@ function SearchProductModal({ visible, closeModal, onClickSelect }: Props) {
   return (
     <div
       css={css`
-        z-index: 2;
+        z-index: 3;
       `}
     >
       <TurtleContentModal
