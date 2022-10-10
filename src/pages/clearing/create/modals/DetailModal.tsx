9e751c4d8c58c24aa568d3c316598efaa2c1812a
gangpyono <shrkdvy123@gmail.com>
2022-10-10 17:19:22 +0900

    feat  : 거래처별 결제 상세내용 모달생성

---
+++ src/pages/clearing/create/modals/DetailModal.tsx
@@ -0,0 +1,201 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import { TurtleDivider, TurtleIcon } from '@components/element';
+import { ClearingInfo } from '@apis/clearingAPI';
+
+interface Props {
+  visible: boolean;
+  onClose: () => void;
+  selectedRow: ClearingInfo;
+}
+
+function DetailModal({
+  visible = false,
+  onClose,
+
+  selectedRow,
+}: Props) {
+  return (
+    <>
+      {visible && (
+        <div css={modalCss.mask}>
+          <div css={modalCss.container}>
+            <div css={modalCss.header}>
+              <TurtleIcon name="modalClose" onClick={onClose} />
+            </div>
+
+            <div css={modalTopContentCss.self}>
+              <div css={modalTopContentCss.title}>2020-07-19</div>
+              <div css={modalTopContentCss.content}>
+                <div
+                  css={[modalTopContentCss.item, modalTopContentCss.firstItem]}
+                >
+                  <span>거래처명</span>
+                  <span>{selectedRow.vendor_info.vendor_name}</span>
+                </div>
+                <div
+                  css={[modalTopContentCss.item, modalTopContentCss.secondItem]}
+                >
+                  <span>결제요청 금액</span>
+                  <span>
+                    {selectedRow.clearing_amount?.toLocaleString() ?? 0}원
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <TurtleDivider marginTop={30} marginBottom={40} />
+
+            <div>
+              <div css={modalBottomContentCss.title}>상세내역</div>
+
+              <ul css={modalBottomContentCss.content}>
+                <li css={[modalBottomContentCss.item]}>
+                  <span>교환/반품</span>
+                  <span>
+                    -{' '}
+                    {selectedRow.overpaid_payment_amount?.toLocaleString() ?? 0}
+                  </span>
+                </li>
+                <li css={modalBottomContentCss.item}>
+                  <span>미송입고</span>
+                  <span>
+                    - {selectedRow.reserve_subtract_amount.toLocaleString()}
+                  </span>
+                </li>
+                <li css={modalBottomContentCss.item}>
+                  <span>당일미송</span>
+                  <span>
+                    {selectedRow.reserve_payment_amount.toLocaleString()}
+                  </span>
+                </li>
+                <li css={modalBottomContentCss.item}>
+                  <span>당일입고</span>
+                  <span>
+                    {(
+                      selectedRow.warehousing_amount +
+                      selectedRow.reserve_subtract_amount
+                    ).toLocaleString()}
+                  </span>
+                </li>
+                <li css={modalBottomContentCss.item}>
+                  <span>미결제</span>
+                  <span>{selectedRow.unpaid_amount.toLocaleString()}</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+const modalCss = {
+  mask: css({
+    height: '100vh',
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    zIndex: 2,
+    background: 'rgba(0, 0, 0, 0.45)',
+  }),
+
+  container: css({
+    fontSize: 15,
+    lineHeight: 1,
+    padding: 32,
+    height: 597,
+    width: 400,
+
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+
+    display: 'flex',
+    flexDirection: 'column',
+    boxShadow: '0px 8px 28px rgba(34, 44, 56, 0.28)',
+    background: '#fff',
+    borderRadius: 4,
+  }),
+
+  header: css({
+    display: 'flex',
+    justifyContent: 'end',
+  }),
+};
+
+const modalTopContentCss = {
+  self: css({
+    marginTop: 4,
+  }),
+
+  title: css({
+    color: '#A1A2A6',
+    fontWeight: 500,
+    marginBottom: 40,
+  }),
+
+  content: css({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 16,
+  }),
+
+  item: css({
+    display: 'flex',
+    justifyContent: 'space-between',
+    'span:first-child': {
+      color: '#A1A2A6',
+      fontWeight: 500,
+    },
+  }),
+
+  firstItem: css({
+    'span:last-child': {
+      color: '#242934',
+      fontWeight: 700,
+    },
+  }),
+
+  secondItem: css({
+    alignItems: 'center',
+    'span:last-child': {
+      color: '#00B3BE',
+      fontWeight: 700,
+      fontSize: 28,
+    },
+  }),
+};
+
+const modalBottomContentCss = {
+  title: css({
+    color: '#A1A2A6',
+    fontWeight: 500,
+    marginBottom: 24,
+  }),
+
+  content: css({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 16,
+  }),
+
+  item: css({
+    display: 'flex',
+    justifyContent: 'space-between',
+
+    'span:first-child': {
+      color: '#5B5D63',
+      fontWeight: 500,
+    },
+    'span:last-child': {
+      color: '#242934',
+    },
+  }),
+};
+
+export default DetailModal;
