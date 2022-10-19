0e9a95526b21c9f7f498dc27d220a6a4ad1c2fd3
sukjin <sjhan@turtleship.io>
2022-10-19 16:48:20 +0900

    feature: 단건추가 기능 기획변경에 맞게 수정
    유저 입력 그대로 받아서 출력. 시스템에서 임의로 변형하지 않음

--- src/pages/order/create/modals/AddNewOrderModal.tsx
+++ src/pages/order/create/modals/AddNewOrderModal.tsx
@@ -29,7 +29,6 @@ function AddNewOrderModal({ visible, close }: Props) {
       },
     ],
   });
-  const { updateSuccess, findSuccess } = useOrderCart();
   const [floor, setFloor] = useState('');
   //사입삼촌에 등록된 쇼핑몰 목록
   const getPickerStoresQuery = useQuery(
@@ -55,51 +54,6 @@ function AddNewOrderModal({ visible, close }: Props) {
           colon={false}
           labelCol={{ span: 7 }}
           wrapperCol={{ span: 17 }}
-          onFinish={(values) => {
-            if (
-              // 단건 추가하려는 쇼핑몰이 이미 등록되어 있는 경우
-              !findSuccess({
-                rt_store_id: selectStore.id,
-                rt_store_name: selectStore.name,
-                orders: [
-                  {
-                    vendor_name: values.vendor_name,
-                    vendor_address: `${values.vendor_address_buliding} ${values.vendor_address_floor} ${values.vendor_address_col}`,
-                    vendor_mobile: '',
-                    mobile: values.mobile,
-                    product_name: values.vendor_product_name,
-                    product_option: values.option ?? '',
-                    order_type: values.type,
-                    product_price: values.price,
-                    product_count: values.count,
-                    memo: values.memo,
-                    ws_store_info: [],
-                  },
-                ],
-              })
-            ) {
-              //새로운 쇼핑몰 인경우
-              updateSuccess({
-                rt_store_id: selectStore.id,
-                rt_store_name: selectStore.name,
-                orders: [
-                  {
-                    vendor_name: values.vendor_name,
-                    vendor_address: `${values.vendor_address_buliding} ${values.vendor_address_floor} ${values.vendor_address_col}`,
-                    vendor_mobile: '',
-                    mobile: values.mobile,
-                    product_name: values.vendor_product_name,
-                    product_option: values.option ?? '',
-                    order_type: values.type,
-                    product_price: values.price,
-                    product_count: values.count,
-                    memo: values.memo,
-                    ws_store_info: [],
-                  },
-                ],
-              });
-            }
-          }}
         >
           {/* 쇼핑몰 */}
           <Form.Item
