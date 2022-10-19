4e037b634afdd5694db32011abcb10b03ce7b8af
gangpyonoh <gpnoh@turtleship.io>
2022-10-20 00:28:43 +0900

    feat : 툴팁 내용 줄바꿈적용

--- src/components/combine/TooltipColumn.tsx
+++ src/components/combine/TooltipColumn.tsx
@@ -1,15 +1,28 @@
 import { TurtleIcon } from '@components/element';
 import { css } from '@emotion/react';
 import { Tooltip } from 'antd';
+import React from 'react';
 
 interface Props {
-  title: string;
+  title: string[];
   children?: React.ReactNode;
 }
 
 function TooltipColumn({ title, children }: Props) {
   return (
-    <Tooltip title={title} css={tooltip}>
+    <Tooltip
+      title={
+        <React.Fragment>
+          {title.map((item, index) => (
+            <React.Fragment key={index}>
+              {item}
+              <br />
+            </React.Fragment>
+          ))}
+        </React.Fragment>
+      }
+      css={tooltip}
+    >
       <span css={{ marginRight: 4, lineHeight: 1.1 }}>{children}</span>
       <TurtleIcon name="info" />
     </Tooltip>
