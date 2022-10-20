b0e8c6dcb4ce3bb8f3e687ab95097c89fbe1d616
gangpyonoh <gpnoh@turtleship.io>
2022-10-20 17:51:40 +0900

    feat : 아이디찾기 qa반영

--- src/pages/auth/find-id/FindIdForm.tsx
+++ src/pages/auth/find-id/FindIdForm.tsx
@@ -1,7 +1,7 @@
 import { t } from 'i18next';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 import { Button, Form } from 'antd';
 import { PhoneAuthForm } from '@components/combine';
 import userAPI from '@apis/userAPI';
@@ -10,53 +10,45 @@ import { TurtleDivider, TurtleText } from '@components/element';
 import { css } from '@emotion/react';
 
 function FindIdForm() {
-  const queryClient = useQueryClient();
   const navigate = useNavigate();
-  const [phone, setPhone] = useState('');
-  const [token, setToken] = useState('');
+  const [auth, setAuth] = useState({ phone: '', token: '' });
 
   // 아이디 리스트 요청
   const getIDQuery = useQuery(
-    ['getID', phone, token],
-    () => userAPI.getID({ phone, token }),
+    ['getID', auth],
+    () => userAPI.getID({ phone: auth.phone, token: auth.token }),
     {
-      enabled: phone && token ? true : false,
+      enabled: !!auth.phone && !!auth.token,
     },
   );
-
-  // 요청 데이터 초기화
-  useEffect(() => {
-    return () => {
-      queryClient.removeQueries(['getID', phone, token]);
-    };
-  }, [queryClient, phone, token]);
+  console.log(getIDQuery.data);
 
   return (
     <>
       <div css={cardCss.self}>
         <TurtleText css={cardCss.title}>{t('find id')}</TurtleText>
         <TurtleText css={cardCss.subTitle}>
-          {!getIDQuery.data
+          {!auth.token
             ? t('description.please phone auth')
             : '고객님의 정보와 일치하는 아이디 목록입니다.'}
         </TurtleText>
       </div>
 
       <Form layout="vertical">
-        {!getIDQuery.data && (
+        {!auth.token && (
           <PhoneAuthForm
-            onSuccess={(data) => {
-              const { phone, token } = data;
-              setPhone(phone);
-              setToken(token);
+            onSuccess={({ phone, token }) => {
+              setAuth({ phone, token });
             }}
           />
         )}
 
         {getIDQuery.data && (
           <div>
-            {getIDQuery.data.map((item) => (
-              <div css={itemHeight}>{item.login_id}</div>
+            {getIDQuery.data.map((item, index) => (
+              <div css={itemHeight} key={index}>
+                {item.login_id}
+              </div>
             ))}
 
             <Button
@@ -65,11 +57,10 @@ function FindIdForm() {
                 navigate('/');
               }}
             >
-              확인
+              로그인 하러가기
             </Button>
           </div>
         )}
-
         <div css={footerCss.self}>
           <Link to="/" css={footerCss.content}>
             {t('login')}
