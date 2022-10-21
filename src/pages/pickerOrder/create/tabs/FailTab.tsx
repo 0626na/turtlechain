bbb7be6faba7b1bc5d78b1e1cdf2e7ea3e79b95d
sukjin <sjhan@turtleship.io>
2022-10-21 17:29:35 +0900

    feature: 발주 실패탭 리팩토링

--- src/pages/pickerOrder/create/tabs/FailTab.tsx
+++ src/pages/pickerOrder/create/tabs/FailTab.tsx
@@ -1,16 +1,20 @@
-import { StoreOrderItemExcelParsing } from '@apis/orderAPI';
+import React from 'react';
 import {
-  TurtleFormSelect,
+  MemoIcon,
   TurtleTableNumberInput,
   TurtleTableTitle,
 } from '@components/element';
-import TurtleTablePhoneNumberInput from '@components/element/input/TurtleTablePhoneNumberInput';
+import TurtleTableSelect from '@components/element/select/TurtleTableSelect';
 import useModal from '@hooks/useModal';
-import useOrderCart from '@hooks/useOrderCart';
-import { notNumPattern, phonePattern } from '@utils/pattern';
+import useOrderCart, { FailListForOutput } from '@hooks/useOrderCart';
 import { message, Table, TabPaneProps, Tabs } from 'antd';
 import { useState } from 'react';
-
+import OrderMemoModal from '../modals/OrderMemoModal';
+import { category } from './SucceessTab';
+import { t } from 'i18next';
+import { StoreOrder, StoreOrderItemExcelParsing } from '@apis/orderAPI';
+import TurtleTablePhoneNumberInput from '@components/element/input/TurtleTablePhoneNumberInput';
+import { notNumPattern, phonePattern } from '@utils/pattern';
 import AddOrderFailtoSuccessModal from '../modals/AddOrderFailtoSuccessModal';
 
 interface Props extends TabPaneProps {
@@ -18,262 +22,260 @@ interface Props extends TabPaneProps {
 }
 
 function FailTab({ loading, ...props }: Props) {
-  const { cart, setCart, failList, setFailList } = useOrderCart();
-  const [visible, open, close] = useModal();
-  const [failData, setFailData] = useState({
-    data: {
-      id: 0,
-      store_id: 0,
-      store_name: '',
-      vendor_name: '',
-      vendor_address: '',
-      mobile: '',
-      vendor_product: '',
-      product_option: '',
-      type: '',
-      count: '',
-      price: '',
-      memo: '',
-    },
+  const { cart, setCart, failListOutput } = useOrderCart();
+  const [selectRowID, setSelectRowID] = useState(-1);
+  const [failToSuccessRowData, setfailToSuccessRowData] = useState({
+    id: 0,
     mobile: '',
   });
-  return (
-    <Tabs.TabPane {...props}>
-      <AddOrderFailtoSuccessModal
-        visible={visible}
-        onCancel={close}
-        onOk={() => {
-          const failtoSuccessStores = cart.failList
-            .filter((store) => store.rt_store_id === failData.data.store_id)
-            .map<StoreOrderItemExcelParsing>((store) => ({
-              rt_store_id: store.rt_store_id,
-              rt_store_name: store.rt_store_name,
-              orders: store.orders.map((order) => {
-                if (
-                  order.vendor_name === failData.data.vendor_name &&
-                  order.vendor_address === failData.data.vendor_address
-                ) {
-                  return {
-                    ...order,
-                    mobile: failData.mobile.replaceAll('-', ''),
-                  };
-                }
-                return order;
-              }),
-            }));
+  const [memoModalvisible, openMemoModal, closeMemoModal] = useModal();
+  const [
+    failtoSuccessModailvisible,
+    openFailToSuccessModal,
+    closeFailToSuccessModal,
+  ] = useModal();
 
-          setCart({
-            successList: cart.successList.map<StoreOrderItemExcelParsing>(
-              (item) => {
-                const failTosuccessStore = failtoSuccessStores.find(
-                  (store) => store.rt_store_id === item.rt_store_id,
-                );
+  //클릭한 테이블 Row를 찾는 함수
+  const searchSameRow = (
+    store: StoreOrderItemExcelParsing,
+    order: StoreOrder,
+    record: FailListForOutput,
+  ) => {
+    return (
+      store.rt_store_id === record.rt_store_id &&
+      order.order_id === record.order_id
+    );
+  };
 
-                if (failTosuccessStore)
-                  return {
-                    ...item,
-                    orders: item.orders.concat(
-                      ...failTosuccessStore.orders.filter(
-                        (order) => order.mobile !== '',
-                      ),
-                    ),
-                  };
+  //휴대전화번호 Input
+  const failTablePhoneNumberInput = (record: FailListForOutput) => {
+    return (
+      <TurtleTablePhoneNumberInput
+        placeholder="휴대전화번호 입력"
+        maxLength={13}
+        onInput={(e) => {
+          e.currentTarget.value = e.currentTarget.value
+            .replaceAll(notNumPattern, '')
+            .replace(phonePattern, '$1-$2-$3');
 
-                return item;
-              },
-            ),
-            failList: cart.failList,
-          });
-          setFailList(
-            failList.filter(
-              (data) =>
-                data.vendor_name !== failData.data.vendor_name &&
-                data.vendor_address !== failData.data.vendor_address,
-            ),
-          );
-          message.success('성공으로 변경되었습니다.');
-          close();
+          if (e.currentTarget.value.length === 13) {
+            setfailToSuccessRowData({
+              id: record.id,
+              mobile: e.currentTarget.value,
+            });
+
+            openFailToSuccessModal();
+          }
         }}
-        failData={failData}
-        title="성공으로 변환"
-        description={[
-          '올바른 휴대전화번호인가요?',
-          '입력한 휴대전화번호로 발주데이터가 입력됩니다.',
-        ]}
       />
-      <Table
-        scroll={{ x: 1608, y: 398, scrollToFirstRowOnChange: true }}
-        loading={loading}
-        size="small"
-        rowKey={(record) => record.id}
-        dataSource={failList}
-        pagination={{
-          position: ['bottomCenter'],
-          showSizeChanger: false,
+    );
+  };
+
+  return (
+    <>
+      <OrderMemoModal
+        visible={memoModalvisible}
+        close={closeMemoModal}
+        defaultValue={
+          failListOutput().find((item) => item.id === selectRowID)?.memo ?? ''
+        }
+        onOk={(value) => {
+          const clickRow = failListOutput().find(
+            (item) => item.id === selectRowID,
+          );
+          if (clickRow) {
+            setCart({
+              ...cart,
+              failList: cart.failList.map((failItem) => ({
+                ...failItem,
+                orders: failItem.orders.map((order) => ({
+                  ...order,
+                  memo: searchSameRow(failItem, order, clickRow)
+                    ? value
+                    : order.memo,
+                })),
+              })),
+            });
+            message.success(t('message.successMemoInput'));
+            closeMemoModal();
+            return;
+          }
+
+          message.error('데이터를 선택해주세요');
+          closeMemoModal();
         }}
-        title={() => (
-          <TurtleTableTitle totalCount={cart.failList.length ?? 0} />
-        )}
-        columns={[
-          {
-            title: '쇼핑몰',
-            width: '148px',
-            render: (_, record) => record.store_name,
-          },
-          {
-            title: '거래처명',
-            width: '136px',
-            render: (_, record) => record.vendor_name,
-          },
-          {
-            title: '거래처 주소',
-            width: '196px',
-            render: (_, record) => record.vendor_address,
-          },
-          {
-            title: '휴대전화번호',
-            width: '156px',
-            render: (_, record) => {
-              if (
-                record.id !== 0 &&
-                failList[record.id - 1].store_name === record.store_name &&
-                failList[record.id - 1].vendor_name === record.vendor_name &&
-                failList[record.id - 1].vendor_address === record.vendor_address
-              ) {
-                return;
-              }
+      />
+      {failListOutput().length !== 0 && (
+        <AddOrderFailtoSuccessModal
+          title="성공으로 변환"
+          description={[
+            '휴대전화번로를 입력하면 성공으로 변경합니다.',
+            '성공으로 변환한 데이터는 실패탭에서 휴대전화번호가 표시됩니다.',
+          ]}
+          visible={failtoSuccessModailvisible}
+          onCancel={closeFailToSuccessModal}
+          failData={{
+            record: failListOutput()[failToSuccessRowData.id],
+            mobile: failToSuccessRowData.mobile,
+          }}
+          onOk={() => {
+            const failToSuccessRecord =
+              failListOutput()[failToSuccessRowData.id];
+            setCart({
+              ...cart,
+              failList: cart.failList.map((failItem) => ({
+                ...failItem,
+                orders: failItem.orders.map((order) => ({
+                  ...order,
+                  mobile:
+                    failItem.rt_store_id === failToSuccessRecord.rt_store_id &&
+                    order.vendor_name === failToSuccessRecord.vendor_name
+                      ? failToSuccessRowData.mobile
+                      : order.mobile,
+                })),
+              })),
+            });
 
-              return (
-                <TurtleTablePhoneNumberInput
-                  placeholder="휴대전화번호 입력"
-                  onInput={(e) => {
-                    e.currentTarget.value = e.currentTarget.value
-                      .replace(notNumPattern, '')
-                      .replace(phonePattern, '$1-$2-$3');
+            message.success(t('message.success update'));
+            closeFailToSuccessModal();
+          }}
+        />
+      )}
+      <Tabs.TabPane {...props}>
+        <Table
+          scroll={{ x: 1608, y: 'auto', scrollToFirstRowOnChange: true }}
+          loading={loading}
+          size="small"
+          dataSource={failListOutput()}
+          rowKey={(record) => record.id}
+          pagination={{
+            position: ['bottomCenter'],
+            showSizeChanger: false,
+          }}
+          title={() => (
+            <TurtleTableTitle totalCount={failListOutput().length ?? 0} />
+          )}
+          columns={[
+            {
+              title: '쇼핑몰',
+              width: 148,
+              render: (_, record) => record.rt_store_name,
+            },
+            {
+              title: '거래처명',
+              width: 136,
+              render: (_, record) => record.vendor_name,
+            },
+            {
+              title: '거래처 주소',
+              width: 196,
+              render: (_, record) => record.vendor_address,
+            },
+            {
+              title: '휴대전화 번호',
+              width: 156,
+              render: (_, record) => {
+                const tempList = failListOutput();
 
-                    if (e.currentTarget.value.length === 13) {
-                      setFailData({
-                        data: record,
-                        mobile: e.currentTarget.value,
-                      });
-                      open();
-                    }
+                if (record.mobile !== '') return record.mobile;
+                if (record.id === 0) return failTablePhoneNumberInput(record);
+                if (
+                  tempList[record.id - 1].rt_store_name ===
+                    record.rt_store_name &&
+                  tempList[record.id - 1].vendor_name === record.vendor_name &&
+                  tempList[record.id - 1].vendor_address ===
+                    record.vendor_address
+                ) {
+                  return null;
+                }
+
+                return failTablePhoneNumberInput(record);
+              },
+            },
+            {
+              title: '거래처 상품명',
+              width: 216,
+              render: (_, record) => record.product_name,
+            },
+            {
+              title: '옵션',
+              width: 136,
+              render: (_, record) => record.product_option,
+            },
+            {
+              title: '분류',
+              width: 136,
+              render: (_, record) => (
+                <TurtleTableSelect
+                  items={category}
+                  value={record.order_type}
+                  onChange={(value: string) => {
+                    setCart({
+                      successList: cart.successList,
+                      failList: cart.failList.map((failItem) => ({
+                        ...failItem,
+                        orders: failItem.orders.map((order) => ({
+                          ...order,
+                          order_type: searchSameRow(failItem, order, record)
+                            ? value
+                            : order.order_type,
+                        })),
+                      })),
+                    });
                   }}
                 />
-              );
+              ),
             },
-          },
-          {
-            title: '거래처 상품명',
-            width: '216px',
-            render: (_, record) => record.vendor_product,
-          },
-          {
-            title: '옵션',
-            width: '136px',
-            render: (_, record) => record.product_option,
-          },
-          {
-            title: '분류',
-            width: '136px',
-            render: (_, record) => (
-              <TurtleFormSelect
-                items={[
-                  {
-                    value: '발주',
-                    name: '발주',
-                  },
-                  {
-                    value: '미송',
-                    name: '미송',
-                  },
-                  {
-                    value: '반품',
-                    name: '반품',
-                  },
-                  {
-                    value: '교환',
-                    name: '교환',
-                  },
-                  {
-                    value: '샘플',
-                    name: '샘플',
-                  },
-                  {
-                    value: '픽업',
-                    name: '픽업',
-                  },
-                  {
-                    value: '기타',
-                    name: '기타',
-                  },
-                ]}
-                value={record.type}
-                onChange={(value: string) => {
-                  //보여지는 실패케이스 테이블 데이터
-                  setFailList([
-                    ...failList.map((item) => ({
-                      ...item,
-                      type: record.id === item.id ? value : item.type,
-                    })),
-                  ]);
-
-                  //실제로 보내는 데이터
-                  setCart({
-                    successList: cart.successList,
-                    failList: cart.failList.map((item) => ({
-                      ...item,
-                      orders:
-                        item.rt_store_id === record.store_id
-                          ? item.orders.map((order) => ({
-                              ...order,
-                              order_type:
-                                order.vendor_name === record.vendor_name
-                                  ? value
-                                  : order.order_type,
-                            }))
-                          : item.orders,
-                    })),
-                  });
-                }}
-              />
-            ),
-          },
-          {
-            title: '요청 수량',
-            width: '136px',
-            render: (_, record) => (
-              <TurtleTableNumberInput
-                step={1}
-                value={record.count}
-                onChange={(value) => {
-                  setFailList([
-                    ...failList.map((item) => ({
-                      ...item,
-                      count:
-                        record.id === item.id && value !== null
-                          ? value.toString()
-                          : item.count,
-                    })),
-                  ]);
-                }}
-              />
-            ),
-          },
-          {
-            title: '가격',
-            width: '136px',
-            align: 'right',
-            render: (_, record) => record.price,
-          },
-          {
-            title: '메모',
-            width: '212px',
-            render: (_, record) => record.memo,
-          },
-        ]}
-      />
-    </Tabs.TabPane>
+            {
+              title: '수량',
+              width: 136,
+              render: (_, record) => (
+                <TurtleTableNumberInput
+                  value={record.product_count}
+                  step={1}
+                  onChange={(value) => {
+                    setCart({
+                      successList: cart.successList,
+                      failList: cart.failList.map((failItem) => ({
+                        ...failItem,
+                        orders: failItem.orders.map((order) => ({
+                          ...order,
+                          product_count:
+                            searchSameRow(failItem, order, record) &&
+                            value !== null
+                              ? value.toString()
+                              : order.product_count,
+                        })),
+                      })),
+                    });
+                  }}
+                />
+              ),
+            },
+            {
+              title: '가격',
+              width: 136,
+              align: 'right',
+              render: (_, record) => record.product_price,
+            },
+            {
+              title: '메모',
+              align: 'center',
+              width: 100,
+              render: (_, record) => (
+                <MemoIcon
+                  value={record.memo ?? ''}
+                  onClick={() => {
+                    setSelectRowID(record.id);
+                    openMemoModal();
+                  }}
+                />
+              ),
+            },
+          ]}
+        />
+      </Tabs.TabPane>
+    </>
   );
 }
 
