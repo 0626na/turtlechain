aaf22ccb2071b250dc9bae91eff54a4bc328d821
gangpyono <shrkdvy123@gmail.com>
2022-10-30 23:26:39 +0900

    feat : alertMessage 환경설정

--- src/pages/order/history/PageBody.tsx
+++ src/pages/order/history/PageBody.tsx
@@ -10,15 +10,25 @@ import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import DetailModal from './modals/DetailModal';
 import useModal from '@hooks/useModal';
+import { message } from '@utils/message';
 
 function PageBody() {
   const [sheetId, setSheetId] = useState(0);
   const [detailModalVisible, openDetailModal, closeDetailModal] = useModal();
-  const getOrderSheetsQuery = useQuery('getOrderSheetsQuery', () =>
-    orderAPI.getOrderSheets({
-      start_date: moment().subtract(1, 'week').format('YYYY-MM-DD'),
-      end_date: moment().format('YYYY-MM-DD'),
-    }),
+  const getOrderSheetsQuery = useQuery(
+    'getOrderSheetsQuery',
+    () =>
+      orderAPI.getOrderSheets({
+        start_date: moment().subtract(1, 'week').format('YYYY-MM-DD'),
+        end_date: moment().format('YYYY-MM-DD'),
+      }),
+    {
+      onError: () => {
+        message.warn('fdfdf');
+        message.success('fdfdf');
+        message.error('fdfdf');
+      },
+    },
   );
   return (
     <>
