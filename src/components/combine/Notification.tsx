9ad83832e61661cd45e659df94c51f0bb9d39375
정보승 <bsjung@turtleship.io>
2022-11-08 15:02:18 +0900

    fix: notification 페이지이동마다 새로고침안되게 변경

--- src/components/combine/Notification.tsx
+++ src/components/combine/Notification.tsx
@@ -36,8 +36,9 @@ function Notification() {
     () => notificationAPI.get({ type: 'home' }),
     {
       // 1분마다 refetch
-      refetchInterval: 60000,
-      refetchIntervalInBackground: true,
+      refetchInterval: 10000,
+      refetchIntervalInBackground: false,
+      staleTime: 10000,
     },
   );
 
