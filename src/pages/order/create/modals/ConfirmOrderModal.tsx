1855a7aa2e94a2bacf66d62d7f592aacd498d8a4
sukjin <sjhan@turtleship.io>
2022-11-09 16:05:58 +0900

    feature: 쇼핑몰 발주 등록시 컨펌모달. 유저가 선택한 날짜 데이터 사용을 위한 수정

--- src/pages/order/create/modals/ConfirmOrderModal.tsx
+++ src/pages/order/create/modals/ConfirmOrderModal.tsx
@@ -14,7 +14,7 @@ interface Props {
 }
 
 function ConfirmOrderModal({ visible, close }: Props) {
-  const { cart, reset } = useOrderCart();
+  const { cart, date, reset } = useOrderCart();
   const navigate = useNavigate();
 
   //발주서 등록
@@ -76,6 +76,7 @@ function ConfirmOrderModal({ visible, close }: Props) {
                 createOrderItemMutation.mutate({
                   rt_stores: cart.successList.map<OrderItemList>((store) => ({
                     rt_store_id: store.rt_store_id,
+                    request_date: date.date.format('YYYY-MM-DD'),
                     orders: store.orders.map<CreatingOrdersItem>((order) => ({
                       vendor_name: order.vendor_name,
                       vendor_address: order.vendor_address,
