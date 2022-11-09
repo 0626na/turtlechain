71d14561f711f9ca18ae1f6ddc92c418dd8bcd52
sukjin <sjhan@turtleship.io>
2022-11-09 16:03:04 +0900

    feature: 발주 등록시, 유저가 날짜 선택하도록 하는 datepicker 컴포넌트 생성

---
+++ src/components/element/rangePicker/TurtleDatePicker.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { css } from '@emotion/react';
+import { theme } from '@styles/theme';
+import { DatePicker, DatePickerProps } from 'antd';
+import moment from 'moment';
+import useOrderCart from '@hooks/useOrderCart';
+
+function TurtleDatePicker() {
+  const { date, setDate } = useOrderCart();
+
+  return (
+    <div>
+      <DatePicker
+        css={datePicker}
+        value={date.date}
+        onChange={(value) => setDate({ date: moment(value) })}
+        allowClear={false}
+      />
+    </div>
+  );
+}
+
+const datePicker = css({
+  marginLeft: 12,
+  backgroundColor: '#DDF3F5',
+  borderRadius: 8,
+  border: 'none',
+  color: '#00B3BE',
+  '.ant-picker-suffix,input': {
+    color: '#00b3be',
+  },
+});
+
+export default TurtleDatePicker;
