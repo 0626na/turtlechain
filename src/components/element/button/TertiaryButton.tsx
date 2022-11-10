431f3c79730b60457ec68b3b76a693302004b1ec
gangpyonoh <gpnoh@turtleship.io>
2022-11-10 18:03:04 +0900

    feat : 페이플 모달 버튼속성 추가

--- src/components/element/button/TertiaryButton.tsx
+++ src/components/element/button/TertiaryButton.tsx
@@ -5,59 +5,77 @@ import { css } from '@emotion/react';
 interface Props {
   text: string;
   icon?: React.ReactNode;
-
+  size?: 'default' | 'large';
   disabled?: boolean;
   loading?: boolean;
   htmlType?: 'submit';
   onClick?: () => void;
 }
 
-function TertiaryButton({ text, icon, ...props }: Props) {
+const sizeCss = {
+  default: {
+    width: '160px',
+    height: '40px',
+  },
+  large: {
+    width: '100%',
+    height: '48px',
+  },
+};
+
+function TertiaryButton({ text, icon, size = 'default', ...props }: Props) {
   return (
-    <Button css={button} {...props}>
+    <Button
+      css={button}
+      style={{
+        ['--size-width' as string]: sizeCss[size].width,
+        ['--size-height' as string]: sizeCss[size].height,
+      }}
+      {...props}
+    >
       {icon && <div css={iconContainer}>{icon}</div>}
       <TurtleText>{text}</TurtleText>
     </Button>
   );
 }
 
-const button = css`
-  width: 160px;
-  height: 40px;
+const button = css({
+  width: 'var(--size-width)',
+  height: 'var(--size-height)',
 
-  font-weight: 700;
-  border: none;
-  border-radius: 8px;
+  fontWeight: 700,
+  border: 'none',
+  borderRadius: 8,
 
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
+  display: 'inlineFlex',
+  alignItems: 'center',
+  justifyContent: 'center',
 
-  color: #00aab5;
-  stroke: #00aab5;
-  background-color: #ddf3f5;
+  color: '#00aab5',
+  stroke: '#00aab5',
+  backgroundColor: '#ddf3f5',
 
-  &:hover {
-    color: #00aab5;
-    stroke: #00aab5;
-    background-color: #d4e9eb;
-  }
+  '&:hover': {
+    color: '#00aab5',
+    stroke: '#00aab5',
+    backgroundColor: '#d4e9eb',
+  },
 
   // active 상태
-  &.ant-btn:focus {
-    color: #00aab5;
-    stroke: #00aab5;
-    background-color: #ddf3f5;
-    border-color: #ddf3f5;
-  }
+  '&.ant-btn:focus': {
+    color: '#00aab5',
+    stroke: '#00aab5',
+    backgroundColor: '#ddf3f5',
+    borderColor: '#ddf3f5',
+  },
 
-  &.ant-btn[disabled] {
-    color: #00aab5;
-    stroke: #00aab5;
-    background-color: #ddf3f5;
-    opacity: 0.5;
-  }
-`;
+  '&.ant-btn[disabled]': {
+    color: '#00aab5',
+    stroke: '#00aab5',
+    backgroundColor: '#ddf3f5',
+    opacity: 0.5,
+  },
+});
 
 const iconContainer = css`
   margin-right: 5px;
