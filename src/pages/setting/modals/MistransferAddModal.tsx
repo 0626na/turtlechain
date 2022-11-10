3ec64e942c87042e0b3b9ee99a85c300247f4b51
정보승 <bsjung@turtleship.io>
2022-11-10 18:48:02 +0900

    fix: 문자열 수정

--- src/pages/setting/modals/MistransferAddModal.tsx
+++ src/pages/setting/modals/MistransferAddModal.tsx
@@ -56,6 +56,7 @@ function MistransferAddModal({ visible, closeModal }: Props) {
         floor: record.ws_store_id.floor,
         col: record.ws_store_id.col,
         loc: record.ws_store_id.loc,
+        loc_col: `${record.ws_store_id.loc}/${record.ws_store_id.col}`,
         ext: record.ws_store_id.ext,
       },
       vendor_account: {
@@ -151,30 +152,25 @@ function MistransferAddModal({ visible, closeModal }: Props) {
           <Form.Item label={t('table.mobile')} name="vendor_phone">
             <TurtleFormInput
               disabled
-              placeholder="휴대전화번호를 입력해주세요"
+              placeholder="휴대전화 번호를 입력해주세요"
             />
           </Form.Item>
 
           <Form.Item label={t('table.vendorAddress')}>
             <div css={flexGap}>
-              <div css={{ flexBasis: '45%' }}>
+              <div css={{ flexBasis: '50%' }}>
                 <Form.Item name={['vendor_address', 'building']} noStyle>
                   <TurtleFormSelect placeholder="상가" disabled />
                 </Form.Item>
               </div>
-              <div css={{ flexBasis: '15%' }}>
+              <div css={{ flexBasis: '20%' }}>
                 <Form.Item name={['vendor_address', 'floor']} noStyle>
                   <TurtleFormInput placeholder="층" disabled />
                 </Form.Item>
               </div>
-              <div css={{ flexBasis: '15%' }}>
-                <Form.Item name={['vendor_address', 'col']} noStyle>
-                  <TurtleFormInput placeholder="열" disabled />
-                </Form.Item>
-              </div>
-              <div css={{ flexBasis: '25%' }}>
-                <Form.Item name={['vendor_address', 'loc']} noStyle>
-                  <TurtleFormInput placeholder="호" disabled />
+              <div css={{ flexBasis: '30%' }}>
+                <Form.Item name={['vendor_address', 'loc_col']} noStyle>
+                  <TurtleFormInput placeholder="열/호" disabled />
                 </Form.Item>
               </div>
             </div>
