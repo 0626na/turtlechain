0b4d0683eb5ee66df70f5ed1b2765ec6debfd085
gangpyonoh <gpnoh@turtleship.io>
2022-11-10 18:04:40 +0900

    feat : 홈화면 공지 카드

--- src/pages/home/AnnouncementCard.tsx
+++ src/pages/home/AnnouncementCard.tsx
@@ -1,7 +1,19 @@
+import { ArrowRightIcon, TurtleTag } from '@components/element';
+import { theme } from '@styles/theme';
 import React from 'react';
 
 function AnnouncementCard() {
-  return <div>공지카드</div>;
+  return (
+    <>
+      <div css={{ display: 'flex', alignItems: 'center' }}>
+        <TurtleTag color="red">공지</TurtleTag>
+        <div css={{ marginLeft: 16, color: theme.grey800 }}>
+          터틀체인 2.0 업데이트 공지⚡️
+        </div>
+      </div>
+      <ArrowRightIcon value={theme.grey300} />
+    </>
+  );
 }
 
 export default AnnouncementCard;
