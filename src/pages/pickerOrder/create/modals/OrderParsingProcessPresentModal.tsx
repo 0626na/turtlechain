7ebbb0bc414e0f3228fab5d6b101150d2035add8
sukjin <sjhan@turtleship.io>
2022-11-14 11:01:29 +0900

    feature: 발주등록 한번에 추가시 추가하는 발주서중에 에러가 있는경우 에러모달을 출력. 에러가 없는 것들만 발주서 등록

--- src/pages/pickerOrder/create/modals/OrderParsingProcessPresentModal.tsx
+++ src/pages/pickerOrder/create/modals/OrderParsingProcessPresentModal.tsx
@@ -1,6 +1,8 @@
 import { CreateModal, TurtleContentModal } from '@components/combine';
 import { TurtleConfirmModal } from '@components/element';
 import { css } from '@emotion/react';
+import { theme } from '@styles/theme';
+import { t } from 'i18next';
 import React from 'react';
 
 interface Props {
@@ -38,20 +40,71 @@ function OrderParsingProcessPresentModal({
       <div
         css={css({ display: 'flex', flexDirection: 'column', marginTop: 40 })}
       >
-        <div css={css({ display: 'flex' })}>
-          <span css={css({ marginRight: 16 })}>문제 없는 발주서</span>
-          <span>{successCount}개</span>
-        </div>
-        <div css={css({ marginTop: 24 })}>
-          <span css={css({ marginRight: 16 })}>문제 있는 발주서</span>
-          <span>{failCount}개</span>
-        </div>
+        {/* 에러 발주서 갯수 */}
         <div
-          css={css({ display: 'flex', flexDirection: 'column', marginTop: 15 })}
-        >
-          {messages.map((message) => {
-            return <span>{message}</span>;
+          css={css({
+            display: 'flex',
+            marginLeft: 2,
+            color: '#434852',
+            fontWeight: 500,
+            fontSize: 14,
+            letterSpacing: '0.005em',
           })}
+        >
+          <span>{t('order.parsingModal.errorOrdersCount')} </span>
+          <span css={css({ color: '#FA5252', marginLeft: 4, marginRight: 3 })}>
+            {failCount}
+          </span>
+          <span>/ {failCount + successCount}</span>
+        </div>
+
+        {/* 발주서 에러 메세지 */}
+        <div
+          css={css({
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: 17,
+            padding: 14,
+            backgroundColor: '#F0F3F6',
+            borderRadius: 10,
+            fontWeight: 500,
+            fontSize: 14,
+            lineHeight: '140%',
+            letterSpacing: '-0.005em',
+          })}
+        >
+          {messages.map((message, index) => {
+            return (
+              <div key={index}>
+                <span css={css({ color: '#FA5252' })}>
+                  {message.split(':')[0]}:
+                </span>
+                <span>{message.split(':')[1]}</span>
+              </div>
+            );
+          })}
+        </div>
+
+        {/* 예상 에러원인 */}
+        <div
+          css={css({
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: 12,
+            fontSize: 13,
+            backgroundColor: '#383B43',
+            color: theme.grey200,
+            padding: '10px 14px',
+            lineHeight: '160%',
+            letterSpacing: '-0.005em',
+            fontWeight: 400,
+            fontFamily: 'Spoqa Han Sans Neo',
+            borderRadius: 10,
+          })}
+        >
+          <span>{t('order.parsingModal.solution1')}</span>
+          <span>{t('order.parsingModal.solution2')}</span>
+          <span>{t('order.parsingModal.solution3')}</span>
         </div>
       </div>
     </TurtleConfirmModal>
