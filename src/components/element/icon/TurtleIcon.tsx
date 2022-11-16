9a09c62e063ec4e5be6b75e9ec5572fd2404a987
sukjin <sjhan@turtleship.io>
2022-11-16 16:20:38 +0900

    feature: 통계 컴포넌트 크기 조정

--- src/components/element/icon/TurtleIcon.tsx
+++ src/components/element/icon/TurtleIcon.tsx
@@ -53,6 +53,7 @@ import { ReactComponent as MarkIcon } from '@icons/mark.svg';
 import { ReactComponent as AlertSuccessIcon } from '@icons/alertSuccess.svg';
 import { ReactComponent as AlertFailIcon } from '@icons/alertFail.svg';
 import { ReactComponent as AlertWarnIcon } from '@icons/alertWarn.svg';
+import { ReactComponent as AlertWarnRedIcon } from '@icons/alertWarningRed.svg';
 import { ReactComponent as ExclamationMarkIcon } from '@icons/exclamationMark.svg';
 import { ReactComponent as AlertBarArrowRightIcon } from '@icons/alertBarArrowRight.svg';
 import { ReactComponent as TuningIcon } from '@icons/tuning.svg';
