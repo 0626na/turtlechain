f2592cdb6fc653a0689e74b00bbb3bc09347eba7
gangpyono <shrkdvy123@gmail.com>
2022-11-20 15:21:28 +0900

    캐시 테스트

--- src/pages/product/create/modals/AddSingleProductModal.tsx
+++ src/pages/product/create/modals/AddSingleProductModal.tsx
@@ -126,7 +126,7 @@ function AddSingleProductModal({ visible, closeModal }: Props) {
         onClickSelect={selectVendor}
       />
       <TurtleContentModal
-        title={t('product.addSingletest')}
+        title={t('product.addSingletest aaa')}
         visible={visible}
         onClose={closeModal}
       >
