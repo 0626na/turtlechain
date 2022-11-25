0b34f6ae2e5850b9272c1a679df99698eaf87707
sukjin <sjhan@turtleship.io>
2022-11-25 17:18:06 +0900

    fix: 발주등록 페이지 날짜변경 컴포넌트 tooltip 추가

--- src/pages/pickerOrder/create/index.tsx
+++ src/pages/pickerOrder/create/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { t } from 'i18next';
 import { Helmet } from 'react-helmet';
 
@@ -11,9 +11,11 @@ import {
 import TurtleDatePicker from '@components/element/rangePicker/TurtleDatePicker';
 import useOrderCart from '@hooks/useOrderCart';
 import moment from 'moment';
+import { Tooltip } from 'antd';
 
 function PickerOrder() {
   const { cart, setCart } = useOrderCart();
+  const [dateTooltipvisible, setdateToolipVisible] = useState(true);
   return (
     <>
       <Helmet
@@ -22,10 +24,19 @@ function PickerOrder() {
       <PageHeader
         title={t('order.create')}
         button={
-          <TurtleDatePicker
-            date={moment(cart.selectedDate)}
-            onchange={(value) => setCart({ ...cart, selectedDate: value })}
-          />
+          <Tooltip
+            visible={dateTooltipvisible}
+            title="발주요청 일자를 변경할 수 있어요"
+            placement="bottom"
+            zIndex={1}
+          >
+            <div onClick={() => setdateToolipVisible(false)}>
+              <TurtleDatePicker
+                date={moment(cart.selectedDate)}
+                onchange={(value) => setCart({ ...cart, selectedDate: value })}
+              />
+            </div>
+          </Tooltip>
         }
       />
       <PageBody />
