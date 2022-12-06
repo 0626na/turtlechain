dd50f687b8aec99bc339c84fea89cc66547c4db3
sukjin <sjhan@turtleship.io>
2022-12-06 16:36:36 +0900

    hotfix: 구독 이후, 구독 이전 상태가 그대로 남아있는 에러 수정중

--- src/pages/clearing/create/modals/PayMentModal.tsx
+++ src/pages/clearing/create/modals/PayMentModal.tsx
@@ -74,7 +74,10 @@ function PayMentModal({ visible, closeModal }: Props) {
             const check = setInterval(() => {
               getSubscriptionCheckQuery.refetch();
 
-              if (getSubscriptionCheckQuery.isSuccess) {
+              if (
+                getSubscriptionCheckQuery.data?.data.subscription_info
+                  .is_subscribed
+              ) {
                 clearInterval(check);
                 setButtonLoading(false);
                 closeModal();
