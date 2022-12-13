9a0d8a58126b458726a340db2338bd11b48f7352
sukjin <sjhan@turtleship.io>
2022-12-13 10:29:00 +0900

    feedback: 거래처 수정 요청 기능 추가

--- src/pages/pickerVendor/PageBody.tsx
+++ src/pages/pickerVendor/PageBody.tsx
@@ -27,6 +27,7 @@ import { message } from '@utils/message';
 import { t } from 'i18next';
 import React, { useMemo, useState } from 'react';
 import { useMutation, useQuery } from 'react-query';
+import OrderVendorInfoUpdateModal from './modals/OrderVendorInfoUpdateModal';
 
 const options = [
   {
@@ -66,25 +67,56 @@ function PageBody() {
     search_type: 'store_name',
     search_string: '',
     page: 1,
-    page_size: 10,
+    page_size: 17,
   });
+  const [
+    vendorUpdateModalVisible,
+    vendorUpdateModalOpen,
+    vendorUpdateModalClose,
+  ] = useModal();
+  const [selectedRow, setSelectedRow] = useState<WholesalerStore>();
   const getWholesalerStoreListQuery = useQuery(
     [
       'getWholesalerStoreListQuery',
       searchQuery.page,
       searchQuery.search_string,
+      //searchQuery.page,
     ],
     () => wholesalerAPI.getList(searchQuery),
   );
 
+  const filterdList = useMemo(
+    () =>
+      getWholesalerStoreListQuery.data?.data.store_list.filter((store) => {
+        return (
+          store.name.includes(searchQuery.search_string) ||
+          store.building.includes(searchQuery.search_string) ||
+          store.floor.includes(searchQuery.search_string) ||
+          store.col.includes(searchQuery.search_string) ||
+          store.loc.includes(searchQuery.search_string) ||
+          store.ext.includes(searchQuery.search_string) ||
+          store.store_phone[0].phone.includes(searchQuery.search_string) ||
+          store.store_account[0].account_number.includes(
+            searchQuery.search_string,
+          )
+        );
+      }),
+    [getWholesalerStoreListQuery, searchQuery],
+  );
+
   return (
     <>
+      <OrderVendorInfoUpdateModal
+        selectedRow={selectedRow as WholesalerStore}
+        visible={vendorUpdateModalVisible}
+        closeModal={vendorUpdateModalClose}
+      />
       <PageContent>
         <Table
           size="small"
-          scroll={{ y: 648, x: 1608 }}
+          scroll={{ y: 'auto', x: 950 }}
           loading={getWholesalerStoreListQuery.isLoading}
-          dataSource={getWholesalerStoreListQuery.data?.data.store_list ?? []}
+          dataSource={filterdList ?? []}
           rowKey={(record) => record.id}
           pagination={false}
           title={() => (
@@ -108,7 +140,9 @@ function PageBody() {
                   </Col>
                   <Col>
                     <TurtleSearchInput
-                      placeholder={t('please input search query')}
+                      placeholder={t(
+                        'please input vendor name, mobile, account number, account holder',
+                      )}
                       onSearch={(value) =>
                         setSearchQuery({
                           ...searchQuery,
@@ -129,7 +163,6 @@ function PageBody() {
                 total={getWholesalerStoreListQuery.data?.data.total_count ?? 0}
                 showSizeChanger={false}
                 current={searchQuery.page}
-                pageSize={10}
                 onChange={(page) => {
                   setSearchQuery({ ...searchQuery, page });
                 }}
@@ -139,13 +172,11 @@ function PageBody() {
           columns={[
             {
               ellipsis: true,
-              width: 176,
               title: t('table.vendorName'),
               render: (_, record) => record.name,
             },
             {
               ellipsis: true,
-              width: 176,
               title: t('table.vendorAddress'),
               render: (_, record) => {
                 if (record.ext !== '') {
@@ -161,7 +192,6 @@ function PageBody() {
 
             {
               ellipsis: true,
-              width: 176,
               title: t('table.mobile'),
               render: (_, record) => {
                 if (record.store_phone.length !== 0) {
@@ -176,7 +206,6 @@ function PageBody() {
             },
             {
               ellipsis: true,
-              width: 176,
               title: t('table.accountInfo'),
               render: (_, record) => {
                 if (record.store_account.length !== 0)
@@ -186,10 +215,9 @@ function PageBody() {
               },
             },
             {
-              width: 20,
-
               title: '',
               align: 'right',
+              width: 30,
               render: (_, record) => (
                 <TurtleDropdown
                   items={[
@@ -197,7 +225,10 @@ function PageBody() {
                       key: '1',
                       label: t('request info modify'),
                       icon: <TurtleIcon name="updateVendorInfo" />,
-                      onClick: (e) => {},
+                      onClick: (e) => {
+                        setSelectedRow(record);
+                        vendorUpdateModalOpen();
+                      },
                     },
                     {
                       key: '2',
