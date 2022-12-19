9ef64be3abc74c5bef59749fdbdd164e342d5d6f
sukjin <sjhan@turtleship.io>
2022-12-19 18:35:45 +0900

    feature: 쇼핑몰 설정페이지 유저관리 및 결제페이지에서 react-query retry 처리

--- src/pages/clearing/create/modals/PayMentModal.tsx
+++ src/pages/clearing/create/modals/PayMentModal.tsx
@@ -35,7 +35,7 @@ function PayMentModal({ visible, closeModal }: Props) {
         company_id: user?.company_id ?? 0,
       }),
     {
-      enabled: visible && buttonLoading,
+      enabled: visible,
       refetchInterval: (data) => {
         if (data?.data.is_expired) {
           setButtonLoading(false);
