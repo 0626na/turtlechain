b539e0f87ab899d92fa6a913c95ac9613ba83c87
sukjin <sjhan@turtleship.io>
2022-12-22 15:14:14 +0900

    feature: en.json 설정 페이지 부분 및 빠뜨린 부분 영문화 작업

--- src/pages/setting/PageBody.tsx
+++ src/pages/setting/PageBody.tsx
@@ -53,12 +53,15 @@ function PageBody() {
 
           {isStaff ? null : (
             <>
-              <Tabs.TabPane key="user" tab="계정관리">
+              <Tabs.TabPane key="user" tab={t('description.manage accounts')}>
                 <div css={greyContainer}>
                   <UserTab />
                 </div>
               </Tabs.TabPane>
-              <Tabs.TabPane key="company" tab="사업자 관리">
+              <Tabs.TabPane
+                key="company"
+                tab={t('description.manage business info')}
+              >
                 <div css={greyContainer}>
                   <CompanyTab />
                 </div>
@@ -66,7 +69,10 @@ function PageBody() {
             </>
           )}
 
-          <Tabs.TabPane key="mistransfer" tab="오입금 환불">
+          <Tabs.TabPane
+            key="mistransfer"
+            tab={t('description.wrong transfer reversal')}
+          >
             <div css={whiteContainer}>
               <MistransferTab />
             </div>
--- src/pages/warehousing/create/modals/AddSingleProductModal.tsx
+++ src/pages/warehousing/create/modals/AddSingleProductModal.tsx
@@ -64,7 +64,7 @@ function AddSingleProductModal({ visible, closeModal }: Props) {
        *  메인 모달
        */}
       <TurtleContentModal
-        title={t('title.add warehouing')}
+        title={t('title.add warehousing')}
         visible={visible}
         onClose={closeModal}
       >
