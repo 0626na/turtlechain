a8591e77238de7b36b2ee17c8fee670e32fc9ff2
gangpyonoh <gpnoh@turtleship.io>
2023-03-03 16:43:21 +0900

    fix: any 수정, 입고상품검색시 공백 제거, 당일moment 함수로 추출

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,3 +3,7 @@ import moment from 'moment';
 export const aWeekAgo = () => {
   return moment().subtract(1, 'weeks').format('YYYY-MM-DD');
 };
+
+export const today = () => {
+  return moment().format('YYYY-MM-DD');
+};
